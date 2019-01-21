8fc1c60f2a5d7ecd35dac9336187a75464207cb5
Niko Kivel <niko.kivel@psi.ch>
2019-01-21 14:11:41 +0100

    default config EL3602

--- scripts/configureSlave.cmd
+++ scripts/configureSlave.cmd
@@ -6,9 +6,9 @@
 # SLAVE_ID
 # CONFIG, i.e. -Baumer-BMMH30D1G24C1213 !!! with leading hyphen '-' !!!
 
-epicsEnvSet("ECMC_EC_SLAVE_NUM",  "${SLAVE_ID}")
-epicsEnvSet("HW_DESC",            "${HW_DESC}")
+epicsEnvSet("ECMC_EC_SLAVE_NUM" "${SLAVE_ID}")
+epicsEnvSet("HW_DESC"           "${HW_DESC}")
 
-${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}addSlave.cmd, "SLAVE_ID=${SLAVE_ID}, HW_DESC=${HW_DESC}"
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}addSlave.cmd "SLAVE_ID=${SLAVE_ID}, HW_DESC=${HW_DESC}"
 
 ${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}${HW_DESC}${CONFIG}
