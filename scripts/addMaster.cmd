2e90671cc55fe6b4be297f249718be083607f93f
Niko Kivel <niko.kivel@psi.ch>
2019-04-30 09:19:00 +0200

    CoD for addMaster

--- scripts/addMaster.cmd
+++ scripts/addMaster.cmd
@@ -5,10 +5,6 @@
 # MASTER_ID = 0
 
 epicsEnvSet("ECMC_EC_MASTER_ID"          "${MASTER_ID=0}")
-<<<<<<< HEAD
+
 # Choose master
-EthercatMCConfigController ${ECMC_MOTOR_PORT}, "Cfg.EcSetMaster(${ECMC_EC_MASTER_ID})"
-=======
-#Choose master
 ecmcConfigOrDie "Cfg.EcSetMaster(${ECMC_EC_MASTER_ID})"
->>>>>>> CoD
