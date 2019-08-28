d00b67247afabe3cc13eadf26fdf2170693790e5
Niko Kivel <niko.kivel@psi.ch>
2019-08-28 13:48:56 +0200

    ${ECMC_GEN_EC_RECORDS} has no effect anymore

--- scripts/addSlave.cmd
+++ scripts/addSlave.cmd
@@ -8,6 +8,6 @@
 epicsEnvSet("ECMC_EC_SLAVE_NUM",  "${SLAVE_ID}")
 epicsEnvSet("HW_DESC",            "${HW_DESC}")
 
-${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmc${HW_DESC}${ECMC_GEN_EC_RECORDS}.cmd
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmc${HW_DESC}.cmd
 #default records
 ${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}slave.cmd
