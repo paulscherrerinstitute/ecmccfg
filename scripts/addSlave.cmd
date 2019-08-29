86fb1acdbaee660ec7cabe8dae5233af521799af
Niko Kivel <niko.kivel@psi.ch>
2019-08-29 11:06:43 +0200

    auto increment and default value

--- scripts/addSlave.cmd
+++ scripts/addSlave.cmd
@@ -3,11 +3,15 @@
 # Arguments
 # [mandatory]
 # HW_DESC
+# [optional]
 # SLAVE_ID
 
-epicsEnvSet("ECMC_EC_SLAVE_NUM",  "${SLAVE_ID}")
+epicsEnvSet("ECMC_EC_SLAVE_NUM",  "${SLAVE_ID=0}")
 epicsEnvSet("HW_DESC",            "${HW_DESC}")
 
 ${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmc${HW_DESC}.cmd
 #default records
 ${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}slave.cmd
+
+NEXT_SID=${ECMC_EC_SLAVE_NUM}+1
+epicsEnvSet("SLAVE_ID",           "${NEXT_SID}")
