dd84760e7dda836e4a102af75ec28aab10d951ab
Anders Sandström <anders.sandstrom@esss.se>
2019-08-30 15:02:19 +0200

    Add hardware cmd for mcu1025+EL5101+EL3632.

--- hardware/ESS_crates/ecmcMCU1025_plusEL5101_EL3632.cmd
+++ hardware/ESS_crates/ecmcMCU1025_plusEL5101_EL3632.cmd
@@ -1,65 +1,44 @@
 ############################################################
 ############# MCU1025:
 
-epicsEnvSet("ECMC_EC_MASTER_ID"          "0")
-
-#Choose master
-ecmcConfigOrDie "Cfg.EcSetMaster($(ECMC_EC_MASTER_ID))"
-
 #Configure EK1100 coupler terminal
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "0")
-< ../hardware/ecmcEK1100$(ECMC_GEN_EC_RECORDS)
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=0, HW_DESC=EK1100"
 
 #Configure EL1018 digital input terminal
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "1")
-< ../hardware/ecmcEL1018$(ECMC_GEN_EC_RECORDS)
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=1, HW_DESC=EL1018"
 
 #Configure EL2808 digital output terminal
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "2")
-< ../hardware/ecmcEL2808$(ECMC_GEN_EC_RECORDS)
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=2, HW_DESC=EL2808"
 
 # Save the slave number for later
 epicsEnvSet("ECMC_EC_SLAVE_NUM_DIG_OUT", "${ECMC_EC_SLAVE_NUM}")
 
 #Configure EL5002 2-channel SSI encoder terminal, motor 1
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "3")
-< ../hardware/ecmcEL5002$(ECMC_GEN_EC_RECORDS)
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=3, HW_DESC=EL5002"
 
 #Configure Baumer encoder for EL5002 2-channel SSI encoder terminal (Channel 1)
-< ../hardware/ecmcEL5002-Encoder-ch1-Baumer-BMMH30D1G24C1213P55     
+${SCRIPTEXEC} ${ecmccfg_DIR}applySlaveConfig.cmd "CONFIG=-Encoder-ch1-Baumer-BMMH30D1G24C1213P55"
 
 #Configure Baumer encoder for EL5002 2-channel SSI encoder terminal (Channel 2)
-< ../hardware/ecmcEL5002-Encoder-ch2-Baumer-BMMH30D1G24C1213P55     
+${SCRIPTEXEC} ${ecmccfg_DIR}applySlaveConfig.cmd "CONFIG=-Encoder-ch2-Baumer-BMMH30D1G24C1213P55"
 
 # Configure EL9505 Power supply terminal 5V
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "4")
-< ../hardware/ecmcEL9505$(ECMC_GEN_EC_RECORDS)
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=4, HW_DESC=EL9505"
 
 # Configure EL1252 digital input terminal
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "5")
-< ../hardware/ecmcEL1252$(ECMC_GEN_EC_RECORDS)
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=5, HW_DESC=EL1252"
 
 # Configure EL9410 Power supply with refresh of E-Bus.
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "6")
-< ../hardware/ecmcEL9410$(ECMC_GEN_EC_RECORDS)
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=6, HW_DESC=EL9410"
 
 #Configure EL7037 stepper drive terminal, motor 1
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "7")
-< ../hardware/ecmcEL7037$(ECMC_GEN_EC_RECORDS)
-
-#Configure motor for EL7037
-< ../hardware/ecmcEL7037-Motor-Nanotec-ST4118L1804-B
+${SCRIPTEXEC} ${ecmccfg_DIR}configureSlave.cmd, "SLAVE_ID=7, HW_DESC=EL7037, CONFIG=-Motor-Nanotec-ST4118L1804-B"
 
 #Configure EL7037 stepper drive terminal, motor 2
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "8")
-< ../hardware/ecmcEL7037$(ECMC_GEN_EC_RECORDS)
-
-#Configure motor for EL7037
-< ../hardware/ecmcEL7037-Motor-Nanotec-ST4118L1804-B
+${SCRIPTEXEC} ${ecmccfg_DIR}configureSlave.cmd, "SLAVE_ID=8, HW_DESC=EL7037, CONFIG=-Motor-Nanotec-ST4118L1804-B"
 
 #Configure EL5101 counter
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "9")
-< ../hardware/ecmcEL5101$(ECMC_GEN_EC_RECORDS)
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=9, HW_DESC=EL5101"
 
 # Configure as counter card
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,1,1)"
@@ -69,12 +48,14 @@ ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,1,1)"
 epicsEnvSet("ECMC_SAMPLE_RATE_MS_BACK", $(ECMC_SAMPLE_RATE_MS))
 epicsEnvSet("ECMC_SAMPLE_RATE_MS",       "1")
 epicsEnvSet("ECMC_EC_SLAVE_NUM",         "10")
-< ../hardware/ecmcEL3632_s$(ECMC_EC_OVERSAMPLING_FACTOR)$(ECMC_GEN_EC_RECORDS)
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=10, HW_DESC=EL3632_${ECMC_EC_OVERSAMPLING_FACTOR}"
+
 epicsEnvSet("ECMC_SAMPLE_RATE_MS", $(ECMC_SAMPLE_RATE_MS_BACK))
 
 # Sensors ch 0
 epicsEnvSet("ECMC_EC_CHANNEL_ID",  "0")
-< ../hardware/ecmcEL3632-Sensor-chX-MTS-PCB-608A11
+
+${SCRIPTEXEC} ${ecmccfg_DIR}applySlaveConfig.cmd "CONFIG=-Sensor-chX-MTS-PCB-608A11"
 
 # test sensor amplifier (gain = 20)
 epicsEnvSet("ECMC_EC_SDO_INDEX",  "0x80$(ECMC_EC_CHANNEL_ID)8")
@@ -85,7 +66,7 @@ ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},${ECMC_EC_SDO_INDEX},$(ECMC_E
 
 # Sensors ch 1
 epicsEnvSet("ECMC_EC_CHANNEL_ID",  "1")
-< ../hardware/ecmcEL3632-Sensor-chX-MTS-PCB-608A11
+${SCRIPTEXEC} ${ecmccfg_DIR}applySlaveConfig.cmd "CONFIG=-Sensor-chX-MTS-PCB-608A11"
 
 # test sensor amplifier (gain = 20)
 epicsEnvSet("ECMC_EC_SDO_INDEX",  "0x80$(ECMC_EC_CHANNEL_ID)8")
