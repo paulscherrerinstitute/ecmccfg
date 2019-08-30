94afbbc2741658f5e702aebd78b0b6ec2257f997
Anders Sandström <anders.sandstrom@esss.se>
2019-08-30 13:08:29 +0200

    addDataStorage.cmd: Add DS_DEBUG var to enable printouts. Update macro () to {}.

--- scripts/addDataStorage.cmd
+++ scripts/addDataStorage.cmd
@@ -13,13 +13,16 @@
 #   -1  : Update records with plc_function (ds_push_asyn())
 #   0   : Update records when data storage changes.
 #   1.. : Sample rate in ms.
+#  DS_DEBUG : 0
+#   0: No debug printouts.
+#   1: Debug  printouts.
 
 epicsEnvSet("ECMC_STORAGE_INDEX",   "${DS_ID=0}")
 epicsEnvSet("ECMC_STORAGE_SIZE",    "${DS_SIZE}")
 
-ecmcConfigOrDie "Cfg.CreateStorage($(ECMC_STORAGE_INDEX),$(ECMC_STORAGE_SIZE),${DS_TYPE=0})"
-ecmcConfigOrDie "Cfg.ClearStorage($(ECMC_STORAGE_INDEX))"
-ecmcConfigOrDie "Cfg.SetStorageEnablePrintouts($(ECMC_STORAGE_INDEX),1)"
+ecmcConfigOrDie "Cfg.CreateStorage(${ECMC_STORAGE_INDEX},${ECMC_STORAGE_SIZE},${DS_TYPE=0})"
+ecmcConfigOrDie "Cfg.ClearStorage(${ECMC_STORAGE_INDEX})"
+ecmcConfigOrDie "Cfg.SetStorageEnablePrintouts(${ECMC_STORAGE_INDEX},${DS_DEBUG=0})"
 
 # T_SMP_MS=-1 (trigg data transfer with ds_push_asyn())
-dbLoadRecords("ecmcDS.template", "P=$(ECMC_PREFIX), PORT=$(ECMC_ASYN_PORT), ADDR=$(ECMC_ASYN_ADDR), TIMEOUT=$(ECMC_ASYN_TIMEOUT),A=0,Index=$(ECMC_STORAGE_INDEX),NELM=$(ECMC_STORAGE_SIZE),T_SMP_MS=${SAMPLE_RATE_MS=1}")
+dbLoadRecords("ecmcDS.template", "P=${ECMC_PREFIX}, PORT=${ECMC_ASYN_PORT}, ADDR=${ECMC_ASYN_ADDR}, TIMEOUT=${ECMC_ASYN_TIMEOUT},A=0,Index=${ECMC_STORAGE_INDEX},NELM=${ECMC_STORAGE_SIZE},T_SMP_MS=${SAMPLE_RATE_MS=1}")
