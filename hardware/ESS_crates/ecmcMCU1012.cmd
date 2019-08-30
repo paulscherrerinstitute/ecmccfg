8fcb8daaddad72940381ce584af437435ae57b23
Anders Sandström <anders.sandstrom@esss.se>
2019-08-30 09:32:43 +0200

    ecmcMCU1012: Add slave number

--- hardware/ESS_crates/ecmcMCU1012.cmd
+++ hardware/ESS_crates/ecmcMCU1012.cmd
@@ -2,38 +2,30 @@
 ############# MCU1012:
 
 #Configure EK1100 coupler terminal
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "0")
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "HW_DESC=EK1100"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=0, HW_DESC=EK1100"
 
 #Configure EL1018 digital input terminal
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "1")
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "HW_DESC=EL1018"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=1, HW_DESC=EL1018"
 
 #Configure EL2808 digital output terminal
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "2")
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "HW_DESC=EL2808"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=2, HW_DESC=EL2808"
 
 # Save the slave number for later
 epicsEnvSet("ECMC_EC_SLAVE_NUM_DIG_OUT", "${ECMC_EC_SLAVE_NUM}")
 
 #Configure EL5101 incremental encoder terminal, motor 1
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "3")
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "HW_DESC=EL5101"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=3, HW_DESC=EL5101"
 
 # Configure EL9505 Power supply terminal 5V
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "4")
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "HW_DESC=EL9505"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=4, HW_DESC=EL9505"
 
 # Configure EL1252 digital input terminal
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "5")
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "HW_DESC=EL1252"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=5, HW_DESC=EL1252"
 
 # Configure EL9410 Power supply with refresh of E-Bus.
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "6")
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "HW_DESC=EL9410"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd, "SLAVE_ID=6, HW_DESC=EL9410"
 
 #Configure EL7037 stepper drive terminal, motor 1
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "7")
 ${SCRIPTEXEC} ${ecmccfg_DIR}configureSlave.cmd, "SLAVE_ID=7, HW_DESC=EL7037, CONFIG=-Motor-Nanotec-ST4118L1804-B"
 
 #Apply hardware configuration
