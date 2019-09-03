2fd349d6221125b86151e1e2f826271df5e79ed2
Anders Sandström <anders.sandstrom@esss.se>
2019-09-03 09:45:25 +0200

    Replace < ../hardware

--- hardware/Technosoft_slaves/ecmciPOS4808-Init.cmd
+++ hardware/Technosoft_slaves/ecmciPOS4808-Init.cmd
@@ -1,10 +1,4 @@
 ############################################################
-############# Init
-
-< ../general/require_E3
-< ../general/init
-
-############################################################
 ############# General:
 # This file is only used to download the basic setup to an technosoft drive.
 # Normally this is done from Technosoft EasySetup or EasyMotion Studio.
@@ -13,82 +7,50 @@
 # This procedure should only be needed to perform once (new drive or replacing of drive)
 # Please ensure that the online cheksum corresponds to the offline checksum (see logfile printouts)
 ############################################################
-############# ASYN Configuration:
+# ECMC config
+# Arguments
+# [set by module]
+# ECMC_CONFIG_ROOT  ==  $(ecmccfg_DIR)
+# ECMC_CONFIG_DB    ==  $(ECMC_config_TEMPLATES)
+# [mandatory]
+# SYS
+# [optional]
+# ECMC_VER        = 6.0.0
+# EthercatMC_VER  = 3.0.0
+# INIT            = initAll
+# MASTER_ID       = 0
+#
+# calls initAll by default, which calls init
+# calls 'add_master.cmd'
+#require ecmccfg develop "SYS=STEST_KN82,SCRIPTEXEC=runScript"
 
-epicsEnvSet("ECMC_MOTOR_PORT",    "$(SM_MOTOR_PORT=MCU1)")
-epicsEnvSet("ECMC_ASYN_PORT",     "$(SM_ASYN_PORT=MC_CPU1)")
-epicsEnvSet("ECMC_PREFIX",        "$(SM_ECMC_PREFIX=IOC2:)")
+epicsEnvSet("IOC" ,"${IOC="IOC_TEST"}")
 
-ecmcAsynPortDriverConfigure($(ECMC_ASYN_PORT),1000,0,0,100)
+require ecmccfg develop
 
-asynOctetSetOutputEos("$(ECMC_ASYN_PORT)", -1, ";\n")
-asynOctetSetInputEos("$(ECMC_ASYN_PORT)", -1, ";\n")
+# run module startup.cmd
+iocshLoad ${ecmccfg_DIR}startup.cmd, "IOC=${IOC},ECMC_VER=6.0.0,EthercatMC_VER=3.0.0,stream_VER=2.7.14p"
 
-asynSetTraceMask("$(ECMC_ASYN_PORT)", -1, 0x41)
-asynSetTraceIOMask("$(ECMC_ASYN_PORT)", -1, 6)
-asynSetTraceInfoMask("$(ECMC_ASYN_PORT)", -1, 1)
+##############################################################################
+## Config hardware:
 
-EthercatMCCreateController("$(ECMC_MOTOR_PORT)", "$(ECMC_ASYN_PORT)", "32", "200", "1000", "")
-
-############################################################
-############# Misc settings:
-# Disable function call trace printouts
-ecmcConfigOrDie "Cfg.SetEnableFuncCallDiag(0)"
-
-# Disable on change printouts from objects (for easy logging)
-ecmcConfigOrDie "Cfg.SetTraceMaskBit(15,1)"
-
-# Disable on command transform diag
-ecmcConfigOrDie "Cfg.SetTraceMaskBit(7,0)"
-
-# Choose to generate EPICS-records for EtherCAT-entries 
-# (For records use ECMC_GEN_EC_RECORDS="-records" otherwise ECMC_GEN_EC_RECORDS="") 
-epicsEnvSet("ECMC_GEN_EC_RECORDS",          "-records")
-
-# Choose to generate EPICS-records for ax-entries (PosAct, SetPos,..)
-# (For records use ECMC_GEN_AX_RECORDS="-records" otherwise ECMC_GEN_AX_RECORDS="") 
-epicsEnvSet("ECMC_GEN_AX_RECORDS",          "-records")
-
-
-# Update records in 10ms (100Hz) 
-epicsEnvSet("ECMC_SAMPLE_RATE_MS",       "10")
-
-############################################################
-############# Configure hardware:
-
-epicsEnvSet("ECMC_EC_MASTER_ID"          "0")
-
-#Choose master
-ecmcConfigOrDie "Cfg.EcSetMaster($(ECMC_EC_MASTER_ID))"
-
-# Configure IPOS 8020 Drive terminal
-epicsEnvSet("ECMC_EC_SLAVE_NUM",              "42")
-
-< ../hardware/ecmciPOS4808${ECMC_GEN_EC_RECORDS}
-
-# Configure Motor
-< ../hardware/ecmciPOS4808-Motor-McLennan-34HT18C340-Parallel
+${SCRIPTEXEC} ${ecmccfg_DIR}configureSlave.cmd, "SLAVE_ID=42, HW_DESC=IPOS4808, CONFIG=-Motor-McLennan-34HT18C340-Parallel"
 
 # Download base config (parsed from Techosoft EasySetup configuration): Only needed first time a drive is used..
-< ../hardware/ecmciPOS4808-EEPROM-BaseConfig-StepperOpenLoopCSV
+${SCRIPTEXEC} ${ecmccfg_DIR}ecmciPOS4808-EEPROM-BaseConfig-StepperOpenLoopCSV.cmd
 
 # Apply hardware configuration
 ecmcConfigOrDie "Cfg.EcApplyConfig(1)"
 
 ##############################################################################
-############# Diagnostics:
+############# Configure diagnostics:
 
 ecmcConfigOrDie "Cfg.EcSetDiagnostics(1)"
-ecmcConfigOrDie "Cfg.EcSetDomainFailedCyclesLimit(10)"
 ecmcConfigOrDie "Cfg.EcEnablePrintouts(0)"
+ecmcConfigOrDie "Cfg.EcSetDomainFailedCyclesLimit(100)"
+ecmcConfigOrDie "Cfg.SetDiagAxisIndex(1)"
+ecmcConfigOrDie "Cfg.SetDiagAxisFreq(2)"
 ecmcConfigOrDie "Cfg.SetDiagAxisEnable(0)"
 
-##############################################################################
-############# Load general controller level records:
-
-< ../general/general
-
-##############################################################################
-############# Go to runtime:
-
-ecmcConfig "Cfg.SetAppMode(1)"
+# go active
+$(SCRIPTEXEC) ($(ecmccfg_DIR)setAppMode.cmd)
