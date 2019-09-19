dbe9fc68787e8cba8672dd21a1b1b4a0303966fc
Anders Sandström <anders.sandstrom@esss.se>
2019-09-19 08:49:27 +0200

    Add possability to use macros in plc files.
    Plc files are parsed with msi.
    Macros are defined in an optional variable in loadPlcFile.cmd
    
    The idea is to allow generation of generic plc by the use of macros, i.e:
    - Axis id
    - Plc id
    - Debug variable to prevent printouts
    can be defined as variables instead of "hard coded".

--- scripts/loadPLCFile.cmd
+++ scripts/loadPLCFile.cmd
@@ -1,6 +1,6 @@
 #==============================================================================
 # loadPLCFile.cmd
-#- Arguments: FILE, [PLC_ID], [SAMPLE_RATE_MS]
+#- Arguments: FILE, [PLC_ID], [SAMPLE_RATE_MS], [PLC_MACROS]
 
 #-d /**
 #-d   \brief Script for adding a PLC from file.
@@ -10,6 +10,7 @@
 #-d   \param FILE PLC definition file, i.e. ./plc/homeSlit.plc
 #-d   \param PLC_ID (optional) PLC number, default 0
 #-d   \param SAMPLE_RATE_MS (optional) excecution rate, default 1 ms (realtime)
+#-d   \param PLC_MACROS (optional) Substitution macros for PLC code
 #-d   \note Example call:
 #-d   \code
 #-d     ${SCRIPTEXEC} ${ecmccfg_DIR}loadPLCFile.cmd, "PLC_ID=0, FILE=./plc/homeSlit.plc, SAMPLE_RATE_MS=100"
@@ -20,7 +21,12 @@
 
 epicsEnvSet("ECMC_PLC_ID",          "${PLC_ID=0}")
 epicsEnvSet("ECMC_PLC_SAMPLE_RATE_MS","${SAMPLE_RATE_MS=1}") # execute at 1000Hz
+epicsEnvSet("ECMC_PLC_MACROS","${PLC_MACROS=EMPTY}")
+
+#Convert file with optional macros (msi)
+system "msi -V -M ${ECMC_PLC_MACROS} ${FILE} > ${FILE}_msi"
 ecmcConfigOrDie "Cfg.CreatePLC(${ECMC_PLC_ID},${ECMC_PLC_SAMPLE_RATE_MS})"
-ecmcConfigOrDie "Cfg.LoadPLCFile(${ECMC_PLC_ID},${FILE})"
+ecmcConfigOrDie "Cfg.LoadPLCFile(${ECMC_PLC_ID},${FILE}_msi)"
 
 dbLoadRecords("ecmcPlc.db", "PORT=${ECMC_ASYN_PORT},A=0,Index=${ECMC_PLC_ID},Name=${ECMC_PREFIX}")
+
