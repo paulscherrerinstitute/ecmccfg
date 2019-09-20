6d82cf3b9d5c8ca3becf960407e2bbda99fe505b
Niko Kivel <niko.kivel@psi.ch>
2019-09-20 08:40:02 +0200

    macro substitution has no write access to ${MODULE_DIR}. Changed PLC specific file in `/tmp`, with agrument to set tmp dir. Forced the removal since PSI by default aliases `rm` to `rm -i`

--- scripts/loadPLCFile.cmd
+++ scripts/loadPLCFile.cmd
@@ -11,6 +11,7 @@
 #-d   \param PLC_ID (optional) PLC number, default 0
 #-d   \param SAMPLE_RATE_MS (optional) excecution rate, default 1 ms (realtime)
 #-d   \param PLC_MACROS (optional) Substitution macros for PLC code
+#-d   \param TMP_PATH (optional) directory to dump the temporary plc file after macro substitution
 #-d   \note Example call:
 #-d   \code
 #-d     ${SCRIPTEXEC} ${ecmccfg_DIR}loadPLCFile.cmd, "PLC_ID=0, FILE=./plc/homeSlit.plc, SAMPLE_RATE_MS=100"
@@ -19,18 +20,18 @@
 
 #- Create a new PLC 0 (Motion: Direct access through variables, I/O, global var, plc enable)
 
-epicsEnvSet("ECMC_PLC_ID",          "${PLC_ID=0}")
-epicsEnvSet("ECMC_PLC_SAMPLE_RATE_MS","${SAMPLE_RATE_MS=1}") # execute at 1000Hz
-epicsEnvSet("ECMC_PLC_MACROS","${PLC_MACROS=EMPTY}")
+epicsEnvSet("ECMC_PLC_ID",              "${PLC_ID=0}")
+epicsEnvSet("ECMC_PLC_SAMPLE_RATE_MS",  "${SAMPLE_RATE_MS=1}") # execute at 1000Hz
+epicsEnvSet("ECMC_PLC_MACROS",          "${PLC_MACROS=EMPTY}")
+epicsEnvSet("ECMC_TMP_FILE",            "${TMP_PATH=/tmp}/PLC${ECMC_PLC_ID}.plc")
 
-#-Convert file with optional macros (msi)
+#- Convert file with optional macros (msi)
 
-system "msi -V -M ${ECMC_PLC_MACROS} ${FILE} > ${FILE}_msi"
+system "msi -V -M ${ECMC_PLC_MACROS} ${FILE} > ${ECMC_TMP_FILE}"
 ecmcConfigOrDie "Cfg.CreatePLC(${ECMC_PLC_ID},${ECMC_PLC_SAMPLE_RATE_MS})"
-ecmcConfigOrDie "Cfg.LoadPLCFile(${ECMC_PLC_ID},${FILE}_msi)"
+ecmcConfigOrDie "Cfg.LoadPLCFile(${ECMC_PLC_ID},${ECMC_TMP_FILE})"
 
-#-Remove parsed file after load
-system "rm ${FILE}_msi"
+#- Remove parsed file after load
+system "rm -f ${ECMC_TMP_FILE}"
 
 dbLoadRecords("ecmcPlc.db", "PORT=${ECMC_ASYN_PORT},A=0,Index=${ECMC_PLC_ID},Name=${ECMC_PREFIX}")
-
