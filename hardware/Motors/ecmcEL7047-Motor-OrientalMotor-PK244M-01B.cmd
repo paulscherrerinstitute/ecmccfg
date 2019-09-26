07ce7b236f603e477e19fb0760a984ac7b33bc3c
Hürzeler Orell Raphael <huerzeler_o@psi.ch>
2019-09-26 09:59:58 +0200

    added Oriental Motor PK244M-02B/PK244M-01B

---
+++ hardware/Motors/ecmcEL7047-Motor-OrientalMotor-PK244M-01B.cmd
@@ -0,0 +1,27 @@
+#-d /**
+#-d   \brief hardware script for EL7047-Motor-OrientalMotor-PK296M-02B
+#-d   \details Parmetrization of EL7047 for motor Oriental Motor PK296M-02B
+#-d   \author Orell Hürzeler
+#-d   \file
+#-d   \note Max current= 2 A
+#-d   \note Inductance per phase =? mH
+#-d   \note Resistance = 2.25 Ohm
+#-d */
+
+#- Set max current to 1.2A/pase = 4.8A (max current is ? A for this motor)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,4800,2)"
+
+#- Reduced current 1000mA
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,1000,2)"
+
+#- Nominal voltage 4V (unit 10mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,400,2)"
+
+#- Coil resistance 3.3 Ohm per phase (unit 10mOhm)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,330,2)"
+
+#- Motor full steps count 400
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x6,400,2)"
+
+#- Coil inductance 4.3 mH per coil
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,430,2)"
