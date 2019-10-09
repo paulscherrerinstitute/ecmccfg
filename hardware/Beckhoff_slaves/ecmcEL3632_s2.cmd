f6f3a1d7861cb22fef363bc6eaeaceace2fbf6c2
Anders Sandström <anders.sandstrom@esss.se>
2019-10-09 12:46:40 +0200

    Add slave id to all memmap objects (to make uniq).

--- hardware/Beckhoff_slaves/ecmcEL3632_s2.cmd
+++ hardware/Beckhoff_slaves/ecmcEL3632_s2.cmd
@@ -42,5 +42,5 @@ ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID
 epicsEnvSet("ECMC_EC_ARRAY_SIZE"             "2")    # 2 values
 epicsEnvSet("ECMC_EC_ARRAY_BYTE_SIZE"        "4")    # 4 bytes
 
-ecmcConfigOrDie "Cfg.EcAddMemMap(${ECMC_EC_SLAVE_NUM},sm3.p1.e0,$(ECMC_EC_ARRAY_BYTE_SIZE),2,ec$(ECMC_EC_MASTER_ID).mm.CH1_ARRAY)"
-ecmcConfigOrDie "Cfg.EcAddMemMap(${ECMC_EC_SLAVE_NUM},sm3.p52.e0,$(ECMC_EC_ARRAY_BYTE_SIZE),2,ec$(ECMC_EC_MASTER_ID).mm.CH2_ARRAY)"
+ecmcConfigOrDie "Cfg.EcAddMemMap(${ECMC_EC_SLAVE_NUM},sm3.p1.e0,$(ECMC_EC_ARRAY_BYTE_SIZE),2,ec$(ECMC_EC_MASTER_ID).s${ECMC_EC_SLAVE_NUM}.mm.CH1_ARRAY)"
+ecmcConfigOrDie "Cfg.EcAddMemMap(${ECMC_EC_SLAVE_NUM},sm3.p52.e0,$(ECMC_EC_ARRAY_BYTE_SIZE),2,ec$(ECMC_EC_MASTER_ID).s${ECMC_EC_SLAVE_NUM}.mm.CH2_ARRAY)"
