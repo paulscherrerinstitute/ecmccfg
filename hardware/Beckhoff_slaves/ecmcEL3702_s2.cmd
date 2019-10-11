6a4bf69dfe167e927367e787739cf5599cebb83b
Anders Sandström <anders.sandstrom@esss.se>
2019-10-11 10:30:40 +0200

    EL4732,EL3702: Add oversampling factor 2 configs.

---
+++ hardware/Beckhoff_slaves/ecmcEL3702_s2.cmd
@@ -0,0 +1,45 @@
+#-d /**
+#-d   \brief hardware script for EL3702_s2
+#-d   \details 2 channel 16bit analog input differential (with oversampling factor 2)
+#-d   \author Anders Sandstroem
+#-d   \file
+#-d */
+
+#- ###########################################################
+#- ############ Information:
+#-  Description: 2 channel 16bit analog input differential (with oversampling)
+#-  Oversampling factor = 10
+#-
+#- ###########################################################
+
+epicsEnvSet("ECMC_EC_HWTYPE"             "EL3702_s2")
+epicsEnvSet("ECMC_EC_VENDOR_ID"          "0x2")
+epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x0e763052")
+
+ecmcConfigOrDie "Cfg.EcSlaveVerify(0,${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID})"
+
+#- ############ Config PDOS: Channel 1
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1b00,0x6800,0x1,16,CH1_CYCLE_CNT)"
+
+#- Configure PDO:s but do not update individualy. Update is made by mem map object as an array instead (All entries needs to be configuered anyway but cant be accessed individually)
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1a00,0x6000,0x1,16,CH1_VALUE_1,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1a01,0x6010,0x1,16,CH1_VALUE_2,0)"
+
+#- ############ Config PDOS: Channel 2
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,1,0x1b01,0x6800,0x2,16,CH2_CYCLE_CNT)"
+
+#- Configure PDO:s but do not update individualy. Update is made by mem map object as an array instead (All entries needs to be configuered anyway but cant be accessed individually)
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,1,0x1a80,0x6000,0x2,16,CH2_VALUE_1,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,1,0x1a81,0x6010,0x2,16,CH2_VALUE_2,0)"
+
+#-  Configure DC clock 100000ns update for oversampling (10 values oversampling)
+#- NOTE: Not sure why it works with the below settings. Twincat uses 500000 and 1000000. 
+#-       If these settings are used here the period will be to long?!
+#-       Seems one additional value is output (always zero) if twincat settings are used. Need to look into this..
+ecmcConfigOrDie "Cfg.EcSlaveConfigDC(${ECMC_EC_SLAVE_NUM},0x730,500001,0,1000000,0)"
+
+epicsEnvSet("ECMC_EC_ARRAY_SIZE"             "2")    # 10 values
+epicsEnvSet("ECMC_EC_ARRAY_BYTE_SIZE"        "4")    # 20 bytes
+
+ecmcConfigOrDie "Cfg.EcAddMemMap(${ECMC_EC_SLAVE_NUM},CH1_VALUE_1,$(ECMC_EC_ARRAY_BYTE_SIZE),2,ec$(ECMC_EC_MASTER_ID).s${ECMC_EC_SLAVE_NUM}.mm.CH1_ARRAY)"
+ecmcConfigOrDie "Cfg.EcAddMemMap(${ECMC_EC_SLAVE_NUM},CH2_VALUE_1,$(ECMC_EC_ARRAY_BYTE_SIZE),2,ec$(ECMC_EC_MASTER_ID).s${ECMC_EC_SLAVE_NUM}.mm.CH2_ARRAY)"
