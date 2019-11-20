0b93839eb9258a4efc149fd5854bf701258d3596
Anders Sandström <anders.sandstrom@esss.se>
2019-11-20 12:34:35 +0100

    EL6002: Support added. Example with default db and custom db (and plc) added.

---
+++ hardware/Beckhoff_slaves/ecmcEL6002.cmd
@@ -0,0 +1,197 @@
+#-d /**
+#-d   \brief hardware script for EL6002
+#-d   \details 2 channel RS232
+#-d   \author Anders Sandstroem, Niko Kivel
+#-d   \file
+#-d */
+
+#- ###########################################################
+#- ############ Information:
+#-  Description: 2 channel RS232 
+#-  Vendor id   = 0x2  (Beckhoff)
+#-  Product id  = 0x17c03052 (EL6080)
+#-  Direction   = 1,2 (Output/Input)
+#- ###########################################################
+
+epicsEnvSet("ECMC_EC_HWTYPE"             "EL6002")
+epicsEnvSet("ECMC_EC_VENDOR_ID"          "0x2")
+epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x17723052")
+
+ecmcConfigOrDie "Cfg.EcSlaveVerify(0,${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID})"
+
+#- ############  Reset terminal
+ecmcConfigOrDie "Cfg.EcWriteSdo(${ECMC_EC_SLAVE_NUM},0x1011,0x1,1684107116,4)"                                                                
+
+#- ###########################################################
+#- ############ Config PDOS: Inputs
+
+#-SM3: PhysAddr 0x1220, DefaultSize   48, ControlRegister 0x20, Enable 1
+#-  TxPDO 0x1a04 "COM TxPDO-Map Inputs Ch.1"
+#-    PDO entry 0x6001:01, 16 bit, "Status"
+#-    PDO entry 0x6000:11,  8 bit, "Data In 0"
+#-    PDO entry 0x6000:12,  8 bit, "Data In 1"
+#-    PDO entry 0x6000:13,  8 bit, "Data In 2"
+#-    PDO entry 0x6000:14,  8 bit, "Data In 3"
+#-    PDO entry 0x6000:15,  8 bit, "Data In 4"
+#-    PDO entry 0x6000:16,  8 bit, "Data In 5"
+#-    PDO entry 0x6000:17,  8 bit, "Data In 6"
+#-    PDO entry 0x6000:18,  8 bit, "Data In 7"
+#-    PDO entry 0x6000:19,  8 bit, "Data In 8"
+#-    PDO entry 0x6000:1a,  8 bit, "Data In 9"
+#-    PDO entry 0x6000:1b,  8 bit, "Data In 10"
+#-    PDO entry 0x6000:1c,  8 bit, "Data In 11"
+#-    PDO entry 0x6000:1d,  8 bit, "Data In 12"
+#-    PDO entry 0x6000:1e,  8 bit, "Data In 13"
+#-    PDO entry 0x6000:1f,  8 bit, "Data In 14"
+#-    PDO entry 0x6000:20,  8 bit, "Data In 15"
+#-    PDO entry 0x6000:21,  8 bit, "Data In 16"
+#-    PDO entry 0x6000:22,  8 bit, "Data In 17"
+#-    PDO entry 0x6000:23,  8 bit, "Data In 18"
+#-    PDO entry 0x6000:24,  8 bit, "Data In 19"
+#-    PDO entry 0x6000:25,  8 bit, "Data In 20"
+#-    PDO entry 0x6000:26,  8 bit, "Data In 21"
+
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6001,0x1,16,CH1_STATUS)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x11,8,CH1_DATA_IN_0,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x12,8,CH1_DATA_IN_1,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x13,8,CH1_DATA_IN_2,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x14,8,CH1_DATA_IN_3,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x15,8,CH1_DATA_IN_4,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x16,8,CH1_DATA_IN_5,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x17,8,CH1_DATA_IN_6,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x18,8,CH1_DATA_IN_7,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x19,8,CH1_DATA_IN_8,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x1a,8,CH1_DATA_IN_9,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x1b,8,CH1_DATA_IN_10,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x1c,8,CH1_DATA_IN_11,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x1d,8,CH1_DATA_IN_12,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x1e,8,CH1_DATA_IN_13,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x1f,8,CH1_DATA_IN_14,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x20,8,CH1_DATA_IN_15,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x21,8,CH1_DATA_IN_16,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x22,8,CH1_DATA_IN_17,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x23,8,CH1_DATA_IN_18,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x24,8,CH1_DATA_IN_19,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x25,8,CH1_DATA_IN_20,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a04,0x6000,0x26,8,CH1_DATA_IN_21,0)"
+
+
+epicsEnvSet("ECMC_EC_ARRAY_SIZE"             "22")    # 22 values
+epicsEnvSet("ECMC_EC_ARRAY_BYTE_SIZE"        "22")    # 22 bytes
+ecmcConfigOrDie "Cfg.EcAddMemMap(${ECMC_EC_SLAVE_NUM},CH1_DATA_IN_0,$(ECMC_EC_ARRAY_BYTE_SIZE),2,ec$(ECMC_EC_MASTER_ID).s${ECMC_EC_SLAVE_NUM}.mm.CH1_ARRAY_IN)"
+
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6011,0x1,16,CH2_STATUS)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x11,8,CH2_DATA_IN_0,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x12,8,CH2_DATA_IN_1,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x13,8,CH2_DATA_IN_2,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x14,8,CH2_DATA_IN_3,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x15,8,CH2_DATA_IN_4,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x16,8,CH2_DATA_IN_5,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x17,8,CH2_DATA_IN_6,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x18,8,CH2_DATA_IN_7,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x19,8,CH2_DATA_IN_8,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x1a,8,CH2_DATA_IN_9,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x1b,8,CH2_DATA_IN_10,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x1c,8,CH2_DATA_IN_11,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x1d,8,CH2_DATA_IN_12,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x1e,8,CH2_DATA_IN_13,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x1f,8,CH2_DATA_IN_14,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x20,8,CH2_DATA_IN_15,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x21,8,CH2_DATA_IN_16,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x22,8,CH2_DATA_IN_17,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x23,8,CH2_DATA_IN_18,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x24,8,CH2_DATA_IN_19,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x25,8,CH2_DATA_IN_20,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a05,0x6010,0x26,8,CH2_DATA_IN_21,0)"
+epicsEnvSet("ECMC_EC_ARRAY_SIZE"             "22")    # 22 values
+epicsEnvSet("ECMC_EC_ARRAY_BYTE_SIZE"        "22")    # 22 bytes
+ecmcConfigOrDie "Cfg.EcAddMemMap(${ECMC_EC_SLAVE_NUM},CH2_DATA_IN_0,$(ECMC_EC_ARRAY_BYTE_SIZE),2,ec$(ECMC_EC_MASTER_ID).s${ECMC_EC_SLAVE_NUM}.mm.CH2_ARRAY_IN)"
+
+#- ###########################################################
+#- ############ Config PDOS: Outputs
+
+#- RxPDO 0x1604 "COM RxPDO-Map Outputs Ch.1"
+#-    PDO entry 0x7001:01, 16 bit, "Ctrl"
+#-    PDO entry 0x7000:11,  8 bit, "Data Out 0"
+#-    PDO entry 0x7000:12,  8 bit, "Data Out 1"
+#-    PDO entry 0x7000:13,  8 bit, "Data Out 2"
+#-    PDO entry 0x7000:14,  8 bit, "Data Out 3"
+#-    PDO entry 0x7000:15,  8 bit, "Data Out 4"
+#-    PDO entry 0x7000:16,  8 bit, "Data Out 5"
+#-    PDO entry 0x7000:17,  8 bit, "Data Out 6"
+#-    PDO entry 0x7000:18,  8 bit, "Data Out 7"
+#-    PDO entry 0x7000:19,  8 bit, "Data Out 8"
+#-    PDO entry 0x7000:1a,  8 bit, "Data Out 9"
+#-    PDO entry 0x7000:1b,  8 bit, "Data Out 10"
+#-    PDO entry 0x7000:1c,  8 bit, "Data Out 11"
+#-    PDO entry 0x7000:1d,  8 bit, "Data Out 12"
+#-    PDO entry 0x7000:1e,  8 bit, "Data Out 13"
+#-    PDO entry 0x7000:1f,  8 bit, "Data Out 14"
+#-    PDO entry 0x7000:20,  8 bit, "Data Out 15"
+#-    PDO entry 0x7000:21,  8 bit, "Data Out 16"
+#-    PDO entry 0x7000:22,  8 bit, "Data Out 17"
+#-    PDO entry 0x7000:23,  8 bit, "Data Out 18"
+#-    PDO entry 0x7000:24,  8 bit, "Data Out 19"
+#-    PDO entry 0x7000:25,  8 bit, "Data Out 20"
+#-    PDO entry 0x7000:26,  8 bit, "Data Out 21"
+
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7001,0x1,16,CH1_CONTROL)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x11,8,CH1_DATA_OUT_0,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x12,8,CH1_DATA_OUT_1,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x13,8,CH1_DATA_OUT_2,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x14,8,CH1_DATA_OUT_3,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x15,8,CH1_DATA_OUT_4,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x16,8,CH1_DATA_OUT_5,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x17,8,CH1_DATA_OUT_6,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x18,8,CH1_DATA_OUT_7,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x19,8,CH1_DATA_OUT_8,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x1a,8,CH1_DATA_OUT_9,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x1b,8,CH1_DATA_OUT_10,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x1c,8,CH1_DATA_OUT_11,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x1d,8,CH1_DATA_OUT_12,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x1e,8,CH1_DATA_OUT_13,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x1f,8,CH1_DATA_OUT_14,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x20,8,CH1_DATA_OUT_15,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x21,8,CH1_DATA_OUT_16,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x22,8,CH1_DATA_OUT_17,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x23,8,CH1_DATA_OUT_18,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x24,8,CH1_DATA_OUT_19,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x25,8,CH1_DATA_OUT_20,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1604,0x7000,0x26,8,CH1_DATA_OUT_21,0)"
+
+epicsEnvSet("ECMC_EC_ARRAY_SIZE"             "22")    # 22 values
+epicsEnvSet("ECMC_EC_ARRAY_BYTE_SIZE"        "22")    # 22 bytes
+ecmcConfigOrDie "Cfg.EcAddMemMap(${ECMC_EC_SLAVE_NUM},CH1_DATA_OUT_0,$(ECMC_EC_ARRAY_BYTE_SIZE),1,ec$(ECMC_EC_MASTER_ID).s${ECMC_EC_SLAVE_NUM}.mm.CH1_ARRAY_OUT)"
+
+#- ############ Config PDOS: Channel 2
+
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7011,0x1,16,CH2_CONTROL)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x11,8,CH2_DATA_OUT_0,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x12,8,CH2_DATA_OUT_1,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x13,8,CH2_DATA_OUT_2,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x14,8,CH2_DATA_OUT_3,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x15,8,CH2_DATA_OUT_4,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x16,8,CH2_DATA_OUT_5,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x17,8,CH2_DATA_OUT_6,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x18,8,CH2_DATA_OUT_7,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x19,8,CH2_DATA_OUT_8,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x1a,8,CH2_DATA_OUT_9,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x1b,8,CH2_DATA_OUT_10,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x1c,8,CH2_DATA_OUT_11,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x1d,8,CH2_DATA_OUT_12,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x1e,8,CH2_DATA_OUT_13,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x1f,8,CH2_DATA_OUT_14,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x20,8,CH2_DATA_OUT_15,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x21,8,CH2_DATA_OUT_16,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x22,8,CH2_DATA_OUT_17,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x23,8,CH2_DATA_OUT_18,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x24,8,CH2_DATA_OUT_19,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x25,8,CH2_DATA_OUT_20,0)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1605,0x7010,0x26,8,CH2_DATA_OUT_21,0)"
+
+epicsEnvSet("ECMC_EC_ARRAY_SIZE"             "22")    # 22 values
+epicsEnvSet("ECMC_EC_ARRAY_BYTE_SIZE"        "22")    # 22 bytes
+ecmcConfigOrDie "Cfg.EcAddMemMap(${ECMC_EC_SLAVE_NUM},CH2_DATA_OUT_0,$(ECMC_EC_ARRAY_BYTE_SIZE),1,ec$(ECMC_EC_MASTER_ID).s${ECMC_EC_SLAVE_NUM}.mm.CH2_ARRAY_OUT)"
+
+#- DC
+ecmcConfigOrDie "Cfg.EcSlaveConfigDC(${ECMC_EC_SLAVE_NUM},0x0,1000000,0,0,0)"
---
+++ hardware/Sensors/ecmcEL6002-Sensor-chX-default.cmd
@@ -0,0 +1,80 @@
+#-d /**
+#-d   \brief hardware script for EL6002-Sensor-chX-default
+#-d   \details Parmetrization Default SDO settings for EL6002 RS232 loop back test
+#-d   \author Anders Sandstroem
+#-d   \file
+#-d */
+
+############################################################
+############# Parmetrization Default SDO settings for EL6002 chx
+#
+#- Environment variables needed:
+#- -ECMC_EC_SDO_INDEX "0x80n0" where n is channel 0..1
+#- -ECMC_EC_SLAVE_NUM = slave number
+#-
+
+#- Enable RTS
+epicsEnvSet("ECMC_EC_SDO_INDEX_OFFSET",  "0x1")
+epicsEnvSet("ECMC_EC_SDO_SIZE",          "1")
+epicsEnvSet("ECMC_EC_SDO_VALUE",         "0")  #- Default = 1
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},${ECMC_EC_SDO_INDEX},$(ECMC_EC_SDO_INDEX_OFFSET),$(ECMC_EC_SDO_VALUE),$(ECMC_EC_SDO_SIZE))"
+
+#- Enable XON/XOF Tx
+epicsEnvSet("ECMC_EC_SDO_INDEX_OFFSET",  "0x2")
+epicsEnvSet("ECMC_EC_SDO_SIZE",          "1")
+epicsEnvSet("ECMC_EC_SDO_VALUE",         "0")  #- Default = 0
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},${ECMC_EC_SDO_INDEX},$(ECMC_EC_SDO_INDEX_OFFSET),$(ECMC_EC_SDO_VALUE),$(ECMC_EC_SDO_SIZE))"
+
+#- Enable XON/XOF Rx
+epicsEnvSet("ECMC_EC_SDO_INDEX_OFFSET",  "0x3")
+epicsEnvSet("ECMC_EC_SDO_SIZE",          "1")
+epicsEnvSet("ECMC_EC_SDO_VALUE",         "0")  #- Default = 0
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},${ECMC_EC_SDO_INDEX},$(ECMC_EC_SDO_INDEX_OFFSET),$(ECMC_EC_SDO_VALUE),$(ECMC_EC_SDO_SIZE))"
+
+#- Enable send FIFO data continuous
+epicsEnvSet("ECMC_EC_SDO_INDEX_OFFSET",  "0x4")
+epicsEnvSet("ECMC_EC_SDO_SIZE",          "1")
+epicsEnvSet("ECMC_EC_SDO_VALUE",         "0")  #- Default = 0
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},${ECMC_EC_SDO_INDEX},$(ECMC_EC_SDO_INDEX_OFFSET),$(ECMC_EC_SDO_VALUE),$(ECMC_EC_SDO_SIZE))"
+
+#- Enable transfer rate optimization
+epicsEnvSet("ECMC_EC_SDO_INDEX_OFFSET",  "0x5")
+epicsEnvSet("ECMC_EC_SDO_SIZE",          "1")
+epicsEnvSet("ECMC_EC_SDO_VALUE",         "1")  #- Default = 1
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},${ECMC_EC_SDO_INDEX},$(ECMC_EC_SDO_INDEX_OFFSET),$(ECMC_EC_SDO_VALUE),$(ECMC_EC_SDO_SIZE))"
+
+#- Baud rate
+#-    
+#- 300    : 1
+#- 600    : 2
+#- 1200   : 3
+#- 2400   : 4
+#- 4800   : 5
+#- 9600   : 6 Default
+#- 19200  : 7
+#- 38400  : 8
+#- 57600  : 9
+#- 115200 : 10 
+
+epicsEnvSet("ECMC_EC_SDO_INDEX_OFFSET",  "0x11")
+epicsEnvSet("ECMC_EC_SDO_SIZE",          "1")
+epicsEnvSet("ECMC_EC_SDO_VALUE",         "6")  #- Default = 6
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},${ECMC_EC_SDO_INDEX},$(ECMC_EC_SDO_INDEX_OFFSET),$(ECMC_EC_SDO_VALUE),$(ECMC_EC_SDO_SIZE))"
+
+#- Data frame
+#-    
+#- 8N1 : 3  Default
+#- 8E1 : 4
+#- 8O1 : 5
+#- 7N2 : 8
+#- 7E2 : 9
+#- 7O2 : 10
+#- 8N2 : 11
+#- 8E2 : 12
+#- 8O2 : 13
+#- 
+
+epicsEnvSet("ECMC_EC_SDO_INDEX_OFFSET",  "0x15")
+epicsEnvSet("ECMC_EC_SDO_SIZE",          "1")
+epicsEnvSet("ECMC_EC_SDO_VALUE",         "3")  #- Default = 3
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},${ECMC_EC_SDO_INDEX},$(ECMC_EC_SDO_INDEX_OFFSET),$(ECMC_EC_SDO_VALUE),$(ECMC_EC_SDO_SIZE))"
