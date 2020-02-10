8bcd92ffc26b21f119eb7d266d6ce2ec8729f288
Anders Sandström <anders.sandstrom@esss.se>
2020-02-10 10:24:08 +0100

    Unset env vars for configureVirtualAxis.cmd, configureAxis.cmd and applyAxisSynchronization.cmd.

--- scripts/applyAxisSynchronization.cmd
+++ scripts/applyAxisSynchronization.cmd
@@ -1,6 +1,6 @@
 #==============================================================================
 # applyAxisSynchronization.cmd
-#- Arguments: CONFIG
+#- Arguments: [CONFIG], [CLEAR_VARS_CMD]
 
 #-d /**
 #-d   \brief Script for applying axis synchronization
@@ -8,6 +8,7 @@
 #-d   \author Niko Kivel
 #-d   \file
 #-d   \param CONFIG configuration file, i.e. ./cfg/linear_1.sax
+#-d   \param CLEAR_VARS_CMD (optional) Set to "empty" for not clear env vars (if needed vars for later use).
 #-d   \note Example call:
 #-d   \code
 #-d     ${SCRIPTEXEC} ${ecmccfg_DIR}applyAxisSynchronization.cmd, "CONFIG=./cfg/linear_1.sax"
@@ -19,3 +20,5 @@ ${SCRIPTEXEC} ${CONFIG}
 
 #- add axis to ECMC
 ${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmc_axis_sync.cmd
+#- Clear env vars
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}${CLEAR_VARS_CMD="ecmc_axis_sync_unset"}.cmd
