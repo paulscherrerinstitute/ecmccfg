b3ec7589e8baffc3f0f384752998f6241eadff86
Anders Sandström <anders.sandstrom@esss.se>
2020-03-10 14:33:31 +0100

    EL4732: Correct direction for NEXTTIME.

--- hardware/Beckhoff_slaves/ecmcEL4732.cmd
+++ hardware/Beckhoff_slaves/ecmcEL4732.cmd
@@ -18,7 +18,7 @@ epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x127c3052")
 ecmcConfigOrDie "Cfg.EcSlaveVerify(0,${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID})"
 
 #- ############ Next start time
-ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},0,2,0x1a82,0x1d09,0x98,S32,NEXT_TIME)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,2,0x1a82,0x1d09,0x98,S32,NEXT_TIME)"
 
 #- ############ Config PDOS: Channel 1
 ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,0,0x1680,0x7800,0x1,S16,CH1_CYCLE_CNT)"
