d2cd14642010688808ee12abe724de6b6a9da797
Anders Sandström <anders.sandstrom@esss.se>
2020-03-28 15:05:28 +0100

    Add LoadPlugin and use it.

--- scripts/loadPlugin.cmd
+++ scripts/loadPlugin.cmd
@@ -1,6 +1,6 @@
 #==============================================================================
-# loadPlugin.cmd
-#- Arguments: FILE, PLUGIN_ID [CONFIG]
+# pluginLoad.cmd
+#- Arguments: FILE, PLUGIN_ID, [CONFIG], [REPORT]
 
 #-d /**
 #-d   \brief Script for loading a ecmc pluginfrom file.
@@ -9,9 +9,15 @@
 #-d   \param FILE      Filename of plugin shared lib (./ecmcPlugin_Advanced.so)
 #-d   \param PLUGIN_ID Id of plugin to load
 #-d   \param CONFIG (optional) Configuration string sent to plugin at construct
+#-d   \param REPORT (optional) Printout information of loaded plugin if set to "1"
 #-d   \note Example call:
 #-d   \code
 #-d     ${SCRIPTEXEC} ${ecmccfg_DIR}loadPlugin.cmd, "PLUGIN_ID=0,FILE=./ecmcPlugin_Advanced.so,CONFIG='PLUGIN CONFIGS GO HERE '
 #-d   \endcode
 #-d */
+
 ecmcConfigOrDie "Cfg.LoadPlugin(${PLUGIN_ID},${FILE},${CONFIG=""})"
+#- Report if REPORT>0
+ecmcEpicsEnvSetCalcTenary("ECMC_PLUGIN_REPORT", "${REPORT=-1}>0","","#")
+${ECMC_PLUGIN_REPORT}ecmcConfigOrDie "Cfg.ReportPlugin(${PLUGIN_ID})"
+epicsEnvUnset(ECMC_PLUGIN_REPORT);
