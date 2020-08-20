5dfbc62c9efbb1be4cc8e921cfbbf7fec9b2bd3f
Hürzeler Orell Raphael <you@example.com>
2020-08-20 11:21:13 +0200

    created files for integration. EP Module ist not working however, indexes in config have to be mapped in another way.

---
+++ hardware/Beckhoff_slaves/ecmcEP3162-0002.cmd
@@ -0,0 +1,27 @@
+#-d /**
+#-d   \brief hardware script for EP3162-0002
+#-d   \details 2 channel 16bit analog input (0-10V)
+#-d   \author Orell Huerzeler
+#-d   \file
+#-d   \note SDOS
+#-d   \param [out] SDO 0x1011:01 --> 1684107116 \b reset
+#-d */
+
+epicsEnvSet("ECMC_EC_HWTYPE"             "EP3162-0002")
+epicsEnvSet("ECMC_EC_VENDOR_ID"          "0x2")
+epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x0c5a4052")
+
+ecmcConfigOrDie "Cfg.EcSlaveVerify(0,${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID})"
+
+#- ############  Reset terminal
+ecmcConfigOrDie "Cfg.EcWriteSdo(${ECMC_EC_SLAVE_NUM},0x1011,0x1,1684107116,4)"
+
+#- ############ Config PDOS: Channel 1
+
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x3101,0x1,8,CH1_STATUS)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x3101,0x2,16,CH1_VALUE)"
+
+#- ############ Config PDOS: Channel 2
+
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a01,0x3102,0x1,8,CH2_STATUS)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a01,0x3102,0x2,16,CH2_VALUE)"
