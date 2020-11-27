e7ed3e7faf01fbc04f7a1532f9cc376c2200b315
Anders Sandström <anders.sandstrom@esss.se>
2020-11-27 10:30:30 +0100

    Set ECMC_MASTER_ID in startup and use it in setAppMode.cmd. Move load of ec records back to setAppMode.cmd

--- startup.cmd
+++ startup.cmd
@@ -78,8 +78,7 @@ epicsEnvSet("ECMC_EC_SAMPLE_RATE" ,${EC_RATE=1000})
 ecmcEpicsEnvSetCalcTernary(ECMC_MASTER_CMD, "${MASTER_ID=0}>=0", "","#- ")
 ${ECMC_MASTER_CMD} ecmcFileExist("${ECMC_CONFIG_ROOT}addMaster.cmd",1)
 ${ECMC_MASTER_CMD} ${SCRIPTEXEC} "${ECMC_CONFIG_ROOT}addMaster.cmd", "MASTER_ID=${MASTER_ID=0}"
-${ECMC_MASTER_CMD} ecmcFileExist("ecmcEc.db",1,1)
-${ECMC_MASTER_CMD} dbLoadRecords("ecmcEc.db","P=${ECMC_PREFIX},PORT=${ECMC_ASYN_PORT},ADDR=0,TIMEOUT=1,MASTER_ID=${ECMC_EC_MASTER_ID},T_SMP_MS=${ECMC_SAMPLE_RATE_MS},TSE=${ECMC_TSE}")
+epicsEnvSet("ECMC_MASTER_ID" ,${MASTER_ID=0})
 
 #- Set default diag params
 ecmcFileExist("${ECMC_CONFIG_ROOT}setDiagnostics.cmd",1)
