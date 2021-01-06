63f3e6338c0ac90b14994dfc00184ba9b18df01f
Niko Kivel <niko.kivel@psi.ch>
2021-01-06 15:49:57 +0100

    EXX2008 slaves simplified, EPP2008 added

--- hardware/Beckhoff_2XXX/EPP/ecmcEPP2008.cmd
+++ hardware/Beckhoff_2XXX/EPP/ecmcEPP2008.cmd
@@ -1,5 +1,5 @@
 #-d /**
-#-d   \brief hardware script for EP2008-000X
+#-d   \brief hardware script for EP2008-00XX
 #-d   \details 8 channel digital output, 0.5A
 #-d   \details -0001: 8x M8  connector
 #-d   \details -0002: 4x M12 connector
@@ -8,10 +8,8 @@
 #-d   \file
 #-d */
 
-epicsEnvSet("ECMC_EC_HWTYPE"             "EP2008")
+epicsEnvSet("ECMC_EC_HWTYPE"             "EPP2008")
 epicsEnvSet("ECMC_EC_VENDOR_ID"          "0x2")
-epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x07d84052")
-
-ecmcConfigOrDie "Cfg.EcSlaveVerify(0,${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID})"
+epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x64764389")
 
 ${SCRIPTEXEC} ${ecmccfg_DIR}ecmcEX2008.cmd
