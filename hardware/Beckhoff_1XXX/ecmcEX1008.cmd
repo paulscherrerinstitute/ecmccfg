238beb66e84c5b629ebed16ab500bac671188cf4
Niko Kivel <niko.kivel@psi.ch>
2021-01-07 12:40:35 +0100

    Ep1 xxx (#60)
    * adding touch probe feature of EP7211 for limit switch use
    
    * CSP and EP7211 internal velocity Kp to 150
    
    * 8008, registers removed from EP7211-0034, TP and ALL
    
    * allow for local slave configuration
    
    * removed nameplate readout for EP7211, added some motorconfigs
    
    * Update ReadMe.md
    
    * EP1XXX, simple slaves
    
    * EP1258, basic input OK
    
    * EP1258 OK
    
    * EP1816, 16 binary in, and 2 accelerometers
    
    Co-authored-by: anderssandstrom <anders.sandstrom@esss.se>

---
+++ hardware/Beckhoff_1XXX/ecmcEX1008.cmd
@@ -0,0 +1,12 @@
+#- verify slave
+ecmcConfigOrDie "Cfg.EcSlaveVerify(0,${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID})"
+
+#- binary inputs
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1a00,0x6800,0x1,B1,binaryInput01)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1a01,0x6010,0x1,B1,binaryInput02)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1a02,0x6020,0x1,B1,binaryInput03)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1a03,0x6030,0x1,B1,binaryInput04)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1a04,0x6040,0x1,B1,binaryInput05)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1a05,0x6050,0x1,B1,binaryInput06)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1a06,0x6060,0x1,B1,binaryInput07)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,0,0x1a07,0x6070,0x1,B1,binaryInput08)"
