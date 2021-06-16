5d4b1050b175531b96dbf6970e8188a6d507d119
Anders Sandstrom <anders.sandstrom@esss.se>
2021-06-16 10:17:18 +0200

    Update default version of ecmc to 6.3.2

--- startup.cmd
+++ startup.cmd
@@ -39,7 +39,7 @@ on error halt
 #-
 #-------------------------------------------------------------------------------
 #- load required modules
-require ecmc        "${ECMC_VER=6.3.0}"
+require ecmc        "${ECMC_VER=6.3.2}"
 #- Require EthercatMC if used.
 ecmcEpicsEnvSetCalcTernary(ECMC_EXE_CMD, "'${ECMC_MR_MODULE=ecmcMotorRecord}'='EthercatMC'", "require  EthercatMC ${EthercatMC_VER=3.0.2} # Using EthercatMC motor record support.","# Using ecmcMotorRecord motor record support.")
 ${ECMC_EXE_CMD}
