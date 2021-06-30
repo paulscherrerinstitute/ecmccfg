0ecd7b7bb10d26789d962308a94ca5d8f465c23b
Niko Kivel <niko.kivel@psi.ch>
2021-06-30 12:57:55 +0200

    MonoDAQ naming updated

--- hardware/MonoDAQ/ecmcMonoDAQ-E-gMeter.cmd
+++ hardware/MonoDAQ/ecmcMonoDAQ-E-gMeter.cmd
@@ -18,11 +18,11 @@ ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${EC
 ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a01,0x6001,0x02,F32,temperature01)"
 
 #- X axis
-ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a60,0x6060,0x01,F32,accelerometer01)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a60,0x6060,0x01,F32,accelerometerX01)"
 #- Y axis
-ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a61,0x6061,0x01,F32,accelerometer02)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a61,0x6061,0x01,F32,accelerometerY01)"
 #- Z axis
-ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a62,0x6062,0x01,F32,accelerometer03)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a62,0x6062,0x01,F32,accelerometerZ01)"
 
 #- ############ Distributed clocks config:
 #- SYNC0: enabled, cycle time = 1 µs, shift time = 0
@@ -36,6 +36,3 @@ ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8020,0x07,0,2)"
 #- sampling rate
 #- The following sample rates are valid (in samples/second): 3, 6, 12, 25, 50, 100, 125, 200, 250, 500, 1000, 2000, 4000
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8020,0x01,${ECMC_EC_SAMPLE_RATE},2)"
-
-
-
