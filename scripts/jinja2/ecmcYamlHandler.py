f83ddf949f7178d8859a8943ae29236e53254e06
Niko Kivel <niko.kivel@psi.ch>
2021-08-23 08:36:13 +0200

    moved checkForVars to YamlHandler

--- scripts/jinja2/ecmcYamlHandler.py
+++ scripts/jinja2/ecmcYamlHandler.py
@@ -4,8 +4,13 @@ import yaml
 class YamlHandler:
     def __init__(self):
         self.yamlData = None
+        self.hasVariables = False
 
     def loadYamlData(self, file):
         # open yaml file containing the PLC configuration
         with open(file) as f:
             self.yamlData = yaml.load(f, Loader=yaml.FullLoader)
+
+    def checkForVariables(self):
+        if 'var' in self.yamlData:
+            self.hasVariables = True
