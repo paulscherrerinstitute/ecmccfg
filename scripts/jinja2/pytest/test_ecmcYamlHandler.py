ee9e0caff341d1f302ca2c85551469456740dc87
Niko Kivel <niko.kivel@psi.ch>
2021-09-30 15:24:57 +0200

    dependencies and fixture

--- scripts/jinja2/pytest/test_ecmcYamlHandler.py
+++ scripts/jinja2/pytest/test_ecmcYamlHandler.py
@@ -7,43 +7,40 @@ def myHandler():
     return YamlHandler()
 
 @pytest.mark.dependency()
-def test_is_supported_axis_type():
-    handler = YamlHandler()
+def test_is_supported_axis_type(myHandler):
     with pytest.raises(NotImplementedError):
         " not implemented _string_"
-        handler.isSupportedAxisType('NotImplementedError')
+        myHandler.isSupportedAxisType('NotImplementedError')
         " not implemented _int_"
-        handler.isSupportedAxisType(1024)
+        myHandler.isSupportedAxisType(1024)
         " not implemented _double_"
-        handler.isSupportedAxisType(3.14)
+        myHandler.isSupportedAxisType(3.14)
     ''' test for integer cast 0..2'''
     for key in range(3):
-        assert handler.isSupportedAxisType(key) is True
+        assert myHandler.isSupportedAxisType(key) is True
 
 @pytest.mark.dependency()
-def test_check_for_key():
-    handler = YamlHandler()
+def test_check_for_key(myHandler):
     with pytest.raises(ValueError):
-        handler.checkForKey('dummy')
-    assert handler.checkForKey('dummy', data_="{dummy: 0}") is True
-    assert handler.checkForKey('notDummy', data_="{dummy: 0}", optional=True) is False
+        myHandler.checkForKey('dummy')
+    assert myHandler.checkForKey('dummy', data_="{dummy: 0}") is True
+    assert myHandler.checkForKey('notDummy', data_="{dummy: 0}", optional=True) is False
     with pytest.raises(KeyError):
-        handler.checkForKey('notDummy', data_="{dummy: 0}")
+        myHandler.checkForKey('notDummy', data_="{dummy: 0}")
 
 @pytest.mark.dependency(depends=["test_check_for_key"])
 def test_load_yaml_data(myHandler):
     with pytest.raises(FileNotFoundError) as e_info:
         myHandler.loadYamlData('fileNotFound.error')
     assert myHandler.yamlData is None
-    myHandler.loadYamlData('test/yamlHandlerTest.yaml')
+    myHandler.loadYamlData('../test/yamlHandlerTest.yaml')
     assert myHandler.yamlData is not None
     assert myHandler.checkForKey('yaml') is True
 
 @pytest.mark.dependency(depends=["test_check_for_key", "test_is_supported_axis_type"])
-def test_set_ecmc_axis_type():
-    handler = YamlHandler()
+def test_set_ecmc_axis_type(myHandler):
     with pytest.raises(ValueError) as e_info:
-        handler.setEcmcAxisType()
-    for key, value in handler.supportedAxisTypes.items():
-        handler.setEcmcAxisType(key)
-        assert handler.axisType == value
+        myHandler.setEcmcAxisType()
+    for key, value in myHandler.supportedAxisTypes.items():
+        myHandler.setEcmcAxisType(key)
+        assert myHandler.axisType == value
