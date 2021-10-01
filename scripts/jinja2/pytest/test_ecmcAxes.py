91ef5fe7a4d568994db1cccef565d44a98e6c56c
Niko Kivel <niko.kivel@psi.ch>
2021-10-01 15:36:02 +0200

    more axis tests

--- scripts/jinja2/pytest/test_ecmcAxes.py
+++ scripts/jinja2/pytest/test_ecmcAxes.py
@@ -2,27 +2,13 @@ import pytest
 
 from ecmcAxes import EcmcAxis
 
+
 @pytest.fixture
 def myAxis():
     return EcmcAxis('yaml_files/joint.yaml', '../templates/')
 
-def test_set_axis_template(myAxis):
-    myAxis.create()
-    assert myAxis.config.axisType is 1
-    with pytest.raises(KeyError):
-        myAxis.config.setAxisTemplate(-1)  # load 'debug.jinja2'
-    myAxis.config.setAxisTemplate(0) # load 'debug.jinja2'
-    assert myAxis.config.hasVariables
-    ''' render w/o var '''
-    myAxis.config.render(myAxis.yamlData)
-    assert myAxis.config.product.strip() == "# var(foo) -> {{ var.bar }} {% for line in var.baz %} {{ line }}, {% endfor %}"
-    ''' pre-render as new template '''
-    myAxis.config.setTemplate(myAxis.config.render(myAxis.yamlData))
-    ''' render 2nd time with pre-rendered template '''
-    myAxis.config.render(myAxis.yamlData)
-    assert myAxis.config.product.strip() == "# var(foo) -> 3.14  1,  2,  3,  None,"
 
-
+@pytest.mark.dependency()
 def test_create(myAxis):
     myAxis.create()
     assert myAxis.config.axisType is 1
@@ -36,3 +22,21 @@ def test_create(myAxis):
     with pytest.raises(FileNotFoundError):
         newAxis = EcmcAxis('yaml_files/joint.yaml', '../notADir/')
         newAxis = EcmcAxis('yaml_files/notAFile.yaml', '../templates/')
+
+
+@pytest.mark.dependency(depends=["test_create"])
+def test_set_axis_template(myAxis):
+    myAxis.create()
+    assert myAxis.config.axisType is 1
+    with pytest.raises(KeyError):
+        myAxis.config.setAxisTemplate(-1)  # load 'debug.jinja2'
+    myAxis.config.setAxisTemplate(0)  # load 'debug.jinja2'
+    assert myAxis.config.hasVariables
+    ''' render w/o var '''
+    myAxis.config.render(myAxis.yamlData)
+    assert myAxis.config.product.strip() == "# var(foo) -> {{ var.bar }} {% for line in var.baz %} {{ line }}, {% endfor %}"
+    ''' pre-render as new template '''
+    myAxis.config.setTemplate(myAxis.config.render(myAxis.yamlData))
+    ''' render 2nd time with pre-rendered template '''
+    myAxis.config.render(myAxis.yamlData)
+    assert myAxis.config.product.strip() == "# var(foo) -> 3.14  1,  2,  3,  None,"
