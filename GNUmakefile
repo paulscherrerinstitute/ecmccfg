ada844199ef667481cd0b7b6c97dd13ece765b2a
Niko Kivel <niko.kivel@psi.ch>
2021-10-20 11:32:05 +0200

    motorRecord switch in yaml conf

--- GNUmakefile
+++ GNUmakefile
@@ -3,10 +3,10 @@ include $(EPICS_MODULES)/makeUtils/latest/utils.mk
 
 MODULE=ecmccfg
 
-LIBVERSION = master
+LIBVERSION = v7.1_RC1
 
 BUILDCLASSES = Linux
-EXCLUDE_VERSIONS = 3 7.0.3 7.0.4.1 7.0.5
+EXCLUDE_VERSIONS = 3 7.0.3 7.0.4.1
 ARCH_FILTER=RHEL%
 
 SCRIPTS+=startup.cmd
--- scripts/jinja2/test/runJointTest.sh
+++ scripts/jinja2/test/runJointTest.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 . ../venv/bin/activate
-python ../axisYamlJinja2.py -d ./ -o output/j.axis -T ../templates -D testJoint.yaml
+# python ../axisYamlJinja2.py -d ./ -o output/j.axis -T ../templates -D testJoint.yaml
+python ../axisYamlJinja2.py -d ./ -o output/j.axis -T ../templates -D AM8111_CSV_vars.yaml
