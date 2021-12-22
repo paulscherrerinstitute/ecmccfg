92a28b506abede74a00da5dace76b9546650191a
Niko Kivel <niko.kivel@psi.ch>
2021-12-22 13:45:24 +0100

    higher holding current

--- hardware/Motors/ecmcEP7047-1032-Motor-Owis-SM24x.cmd
+++ hardware/Motors/ecmcEP7047-1032-Motor-Owis-SM24x.cmd
@@ -12,7 +12,7 @@
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,500,2)"
 
 #- Reduced current 100mA
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,100,2)"
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,250,2)"
 
 #- Nominal voltage 48V (unit 10mV)
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,4800,2)"
