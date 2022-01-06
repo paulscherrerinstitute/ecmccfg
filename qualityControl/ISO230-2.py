81d7457f5f60f1c3bddbef451af279d8a8824aac
Niko Kivel <niko.kivel@psi.ch>
2022-01-06 16:04:39 +0100

    result equations validated

--- qualityControl/ISO230-2.py
+++ qualityControl/ISO230-2.py
@@ -1,6 +1,8 @@
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
+import yaml
+from jinja2 import Template
 
 '''
 ISO230-2 data evaluation
@@ -28,6 +30,7 @@ class ISO230:
         self.actualBwd = pd.DataFrame()
         # mean unidirectional positional deviation
         self.deviation = pd.DataFrame()
+        self.results = {}
         self.maxB = 0
         self.avgB = 0
         self.RFwd = 0
@@ -52,20 +55,9 @@ class ISO230:
               f'deviation\n' \
               f'{self.deviation}\n' \
               f'==== Eval ====\n' \
-              f'reversal:          {self.maxB}\n' \
-              f'mean reversal:     {self.avgB}\n' \
-              f'repeatability fwd: {self.RFwd}\n' \
-              f'repeatability bwd: {self.RBwd}\n' \
-              f'repeatability max: {self.RAll}\n' \
-              f'E fwd:             {self.EFwd}\n' \
-              f'E bwd:             {self.EBwd}\n' \
-              f'E max:             {self.EAll}\n' \
-              f'M max:             {self.MAll}\n' \
-              f'accuracy fwd:      {self.AFwd}\n' \
-              f'accuracy bwd:      {self.ABwd}\n' \
-              f'accuracy max:      {self.AAll}'
+              f'{yaml.dump(self.results)}\n' \
+              f'{self.results}'
         return str
-
 
     def loadFile(self, fn):
         """
@@ -104,7 +96,13 @@ class ISO230:
             match = re.match(r'TGT_DATA\[(\d*)\]=(.*)', t)
             if match:
                 self.targets[int(match.group(1))] = float(match.group(2))
-                # print(f'found target #{match.group(1)} = {match.group(2)}')
+
+    @staticmethod
+    def sortRefData(data):
+        sorted_data = {}
+        for k, v in sorted(data.items()):
+            sorted_data[k] = dict(sorted(v.items()))
+        return sorted_data
 
     def getRefData(self, dir='FWD'):
         dir = dir.upper()
@@ -113,17 +111,16 @@ class ISO230:
         ref = self.parse(m)
         data = {}
         for r in ref:
-            # REF_DATA_FWD[p, n] = d
-            # REF_DATA_FWD[1, 1] = 15.005181721754034
             match = re.match(mr, r)
             if match:
-                # print(f'found ref pos #{match.group(1)}, n[{match.group(2)}] = {match.group(3)}')
-                p = int(match.group(1))
+                i = int(match.group(1))
+                i = self.targets[i]
+                j = int(match.group(2))
                 d = float(match.group(3))
-                if p not in data:
-                    data[p] = []
-                data[int(match.group(1))].append(d)
-        return data
+                if i not in data:
+                    data[i] = {}
+                data[i][j] = d
+        return self.sortRefData(data)
 
     def getReference(self, dir='ALL'):
         dir = dir.upper()
@@ -139,56 +136,57 @@ class ISO230:
 
     def evalDeviation(self):
         # deviation from target
-        deviationFwd = self.actualFwd - self.targets
-        deviationBwd = self.actualBwd - self.targets
+        deviationFwd = self.actualFwd - self.actualFwd.columns
+        deviationBwd = self.actualBwd - self.actualBwd.columns
         # section 2.10
         self.deviation['xiFwd'] = deviationFwd.mean()
         self.deviation['xiBwd'] = deviationBwd.mean()
         # section 2.11
         self.deviation['xiAll'] = self.deviation[['xiFwd', 'xiBwd']].mean(axis=1)
         # absolute deviation, aka 'reversal', section 2.12
-        abs = self.actualFwd - self.actualBwd
-        self.deviation['Bi'] = abs.mean()
+        absDev = self.actualFwd - self.actualBwd
+        self.deviation['Bi'] = absDev.mean()
         # section 2.13
-        self.maxB = self.deviation['Bi'].abs().max()
+        self.results['maxB'] = self.deviation['Bi'].abs().max()
         # section 2.14
-        self.avgB = self.deviation['Bi'].mean()
+        self.results['avgB'] = self.deviation['Bi'].mean()
         # section 2.15
         self.deviation['siFwd'] = deviationFwd.std()
-        self.deviation['siBwd'] = deviationBwd.mean()
+        self.deviation['siBwd'] = deviationBwd.std()
         # section 2.16
         self.deviation['RiFwd'] = 4 * deviationFwd.std()
         self.deviation['RiBwd'] = 4 * deviationBwd.std()
         # section 2.17
         df = pd.DataFrame()
-        df['t1'] = 2 * self.deviation['siFwd'] + 2 * self.deviation['siBwd'] + self.deviation['Bi']
+        df['t1'] = 2 * self.deviation['siFwd'] + 2 * self.deviation['siBwd'] + self.deviation['Bi'].abs()
         df['t2'] = self.deviation['RiFwd']
         df['t3'] = self.deviation['RiBwd']
         self.deviation['RiAll'] = df.max(axis=1)
         # section 2.18
-        self.RFwd = self.deviation['RiFwd'].max()
-        self.RBwd = self.deviation['RiBwd'].max()
+        self.results['RFwd'] = self.deviation['RiFwd'].max()
+        self.results['RBwd'] = self.deviation['RiBwd'].max()
         # section 2.19
-        self.RAll = self.deviation['RiAll'].max()
+        self.results['RAll'] = self.deviation['RiAll'].max()
         # section 2.20
-        self.EFwd = self.deviation['xiFwd'].max() - self.deviation['xiFwd'].min()
-        self.EBwd = self.deviation['xiBwd'].max() - self.deviation['xiBwd'].min()
+        self.results['EFwd'] = self.deviation['xiFwd'].max() - self.deviation['xiFwd'].min()
+        self.results['EBwd'] = self.deviation['xiBwd'].max() - self.deviation['xiBwd'].min()
         # section 2.21
-        self.EAll = self.deviation[['xiFwd', 'xiBwd']].max().max() - self.deviation[['xiFwd', 'xiBwd']].min().min()
+        self.results['EAll'] = self.deviation[['xiFwd', 'xiBwd']].max().max() - self.deviation[
+            ['xiFwd', 'xiBwd']].min().min()
         # section 2.22
-        self.MAll = self.deviation['xiAll'].max() - self.deviation['xiAll'].min()
+        self.results['MAll'] = self.deviation['xiAll'].max() - self.deviation['xiAll'].min()
         # section 2.23
-        self.AFwd = (self.deviation['xiFwd'] + 2 * self.deviation['siFwd']).max() - \
-                    (self.deviation['xiFwd'] - 2 * self.deviation['siFwd']).min()
-        self.ABwd = (self.deviation['xiBwd'] + 2 * self.deviation['siBwd']).max() - \
-                    (self.deviation['xiBwd'] - 2 * self.deviation['siBwd']).min()
+        self.results['AFwd'] = (self.deviation['xiFwd'] + 2 * self.deviation['siFwd']).max() - \
+                               (self.deviation['xiFwd'] - 2 * self.deviation['siFwd']).min()
+        self.results['ABwd'] = (self.deviation['xiBwd'] + 2 * self.deviation['siBwd']).max() - \
+                               (self.deviation['xiBwd'] - 2 * self.deviation['siBwd']).min()
         # section 2.24
         d = []
         d.append((self.deviation['xiFwd'] + 2 * self.deviation['siFwd']).max())
         d.append((self.deviation['xiBwd'] + 2 * self.deviation['siBwd']).max())
         d.append((self.deviation['xiFwd'] - 2 * self.deviation['siFwd']).min())
         d.append((self.deviation['xiBwd'] - 2 * self.deviation['siBwd']).min())
-        self.AAll = max(d) - min(d)
+        self.results['AAll'] = max(d) - min(d)
 
     def boxPlot(self):
         rf = self.actualFwd - self.targets
@@ -211,9 +209,45 @@ if __name__ == "__main__":
 
     dat.evalDeviation()
 
-    print(dat)
+    # print(dat)
 
+    print(dat.actualFwd)
 
+    print(dat.actualBwd)
+
+    t = Template('reversal of axis:  {{res.maxB|round(res.prec)}}\n'
+                 'mean reversal:     {{res.avgB|round(res.prec)}}\n'
+                 'repeatability fwd: {{res.RFwd|round(res.prec)}}\n'
+                 'repeatability bwd: {{res.RBwd|round(res.prec)}}\n'
+                 'repeatability max: {{res.RAll|round(res.prec)}}\n'
+                 'E fwd:             {{res.EFwd|round(res.prec)}}\n'
+                 'E bwd:             {{res.EBwd|round(res.prec)}}\n'
+                 'E max:             {{res.EAll|round(res.prec)}}\n'
+                 'M max:             {{res.MAll|round(res.prec)}}\n'
+                 'accuracy fwd:      {{res.AFwd|round(res.prec)}}\n'
+                 'accuracy bwd:      {{res.ABwd|round(res.prec)}}\n'
+                 'accuracy max:      {{res.AAll|round(res.prec)}}')
+
+    dat.results['prec']=6
+    print(t.render(res=dat.results))
+
+    # dat.actualFwd.columns=[2,4,6,8,10]
+    # print(dat.actualFwd)
+    # print(dat.deviation['xiFwd'])
+    #
+    # pdf = (dat.actualFwd - dat.targets).T
+    # print(pdf)
+
+    #
+    # x=[]
+    # for p in dat.actualFwd.columns:
+    #     x.append(dat.targets[p])
+    # pdf['x'] = x
+    # ax = pdf.plot(x=pdf.index)
+    # mdf = dat.deviation['xiFwd']
+    # mdf['x'] = x
+    # mdf.plot(x='x', ax=ax)
+    # plt.show()
 
     # print( (dat.actualFwd - dat.actualBwd).mean() )
     # rel = dat.actualFwd - dat.targets
