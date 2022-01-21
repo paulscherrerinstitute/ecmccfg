66c0d47ca157381043b5d75e6998633bc8168101
Niko Kivel <niko.kivel@psi.ch>
2022-01-21 09:36:37 +0100

    ecmccfg yaml-config validator, WIP add tests!

--- scripts/jinja2/ecmcConfigValidator.py
+++ scripts/jinja2/ecmcConfigValidator.py
@@ -1,15 +1,32 @@
 import yaml
 import json
 import sys
-from cerberus import Validator
+import cerberus
 
 import ecmcYamlSchema
 
+class color:
+    """
+    class for colorful printing
+    example: print(f'{color.RED}foo{color.END}')
+    """
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
 
-class Validated:
+
+class DictContainer:
     """
-    data container
+    data container for 'dict'
     """
+
     def __init__(self, data=None):
         if data is None:
             self.data = {}
@@ -27,42 +44,15 @@ class Validated:
     def to_json(self):
         return json.dumps(self.data)
 
-
-class ConfigValidator:
-    """
-    config validator
-    args:
-        document, type 'dict'
-    """
-    def __init__(self, document):
-        self.Schema = ecmcYamlSchema.Schema()
-        self.document = document
-        self.validated = Validated()
-        self.v = Validator(purge_unknown=False, allow_unknown=True)
-
-    def get_axis_type(self):
-        schema = self.Schema.get_schema('axis')
-        if self.v.validate(self.document, schema):
-            return self.v.normalized(self.document)['axis']['type']
-        else:
-            raise NotImplementedError(f'{yaml.dump(self.v.errors)}')
-
-    def validate_axis(self, strict=False):
-        schema = self.Schema.get_schema(self.Schema.axisSchemaDict[self.get_axis_type()])
-        if self.v.validate(self.document, schema):
-            return self.v.normalized(self.document)
-        else:
-            e = yaml.dump(self.v.errors)
-            if strict:
-                raise SyntaxError(e)
-            else:
-                print(e)
-
     def write(self, outfile=None, output_format='yaml'):
+        """
+        write contents of 'DictContainer' to file
+        TODO: should be a DictContainer method and be inherited
+        """
         if output_format.lower() == 'yaml':
-            text = self.validated.to_yaml()
+            text = self.to_yaml()
         elif output_format.lower() == 'json':
-            text = self.validated.to_json()
+            text = self.to_json()
         else:
             raise NotImplementedError(f'unsupported output format, options are [yaml|json]')
 
@@ -77,13 +67,85 @@ class ConfigValidator:
                 out.close()
 
 
-if __name__ == '__main__':
+class ConfigValidator:
+    """
+    config validator
+    args:
+        document, type == 'dict'
+    """
+
+    def __init__(self, document):
+        self.Schema = ecmcYamlSchema.Schema()
+        self.document = document
+        self.validated = DictContainer()
+        self.v = cerberus.Validator(purge_unknown=False, allow_unknown=True)
+
+    @staticmethod
+    def __yaml_dump_error(err, strict=False):
+        """
+        dump Cerberus errors as yaml, with colors
+        raise SyntaxError if 'strict'
+        """
+        e = DictContainer(data=err)
+        msg = f'{color.RED}{color.BOLD}\nError:\n' \
+              f'{color.YELLOW}{e.to_yaml()}{color.END}'
+        if strict:
+            raise SyntaxError(msg)
+        else:
+            print(msg)
+
+    def validate(self, document=None, schema=None, strict=False) -> dict:
+        """
+        validate a document against a schema using Cerberus
+        default to 'self.document'
+        schema can be preset with `class_instance.v.schema =`
+        uses __yaml_dump_error in case of validation fails
+        """
+        if document is None:
+            document = self.document
+        if schema is not None:
+            self.v.schema = schema
+        if self.v.validate(document):
+            return self.v.normalized(document)
+        else:
+            self.__yaml_dump_error(self.v.errors, strict=strict)
+
+    def get_axis_type(self) -> int:
+        """
+        return the integer axis type
+        always "strict" __yaml_dump_error
+        re-raises as NotImplementedError with hint to suppoerted axis types
+        """
+        schema = self.Schema.get_schema('axis')
+        try:
+            return self.validate(schema=schema, strict=True)['axis']['type']
+        except SyntaxError as err:
+            raise NotImplementedError(f'{err}\n'
+                                      f'supported axis types are:\n'
+                                      f'{color.GREEN}{ecmcYamlSchema.supportedAxisTypes.keys()}{color.END}')
+
+    def validate_axis(self, strict=False) -> dict:
+        """
+        validate the complete document against the internal axis type.
+        """
+        schema = self.Schema.get_schema(self.Schema.axisSchemaDict[self.get_axis_type()])
+        return self.validate(schema=schema, strict=strict)
+
+
+def main():
     file = 'pytest/yaml_files/joint_all.yaml'
+    # file = 'pytest/yaml_files/axisTypeNotImplementedError.yaml'
     with open(file) as f:
         doc = yaml.load(f, Loader=yaml.FullLoader)
     v = ConfigValidator(doc)
 
-    v.validated.data = v.validate_axis(strict=False)
+    print(v.get_axis_type())
 
-    print(v.validated)
-    print(type(v.validated))
+    v.validated.data = v.validate_axis(strict=False)
+    # v.validated.data = v.validate_axis(strict=True)
+
+    v.validated.write()
+
+
+if __name__ == '__main__':
+    main()
--- scripts/jinja2/ecmcYamlSchema.py
+++ scripts/jinja2/ecmcYamlSchema.py
@@ -17,138 +17,351 @@ supportedAxisTypes = {
 
 
 class Schema:
-    def __init__(self):
-        self.axisSchemaDict = {
-            0: None,
-            1: ['var',
-                'axis',
-                'epics',
-                'encoder',
-                'drive',
-                'controller'],
-            2: ['var',
-                'axis',
-                'epics',
-                'encoder']
-        }
+    axisSchemaDict = {
+        0: None,
+        1: ['var',
+            'axis',
+            'epics',
+            'encoder',
+            'drive',
+            'controller',
+            'trajectory',
+            'input',
+            'plc',
+            'homing',
+            'softlimits',
+            'monitoring'],
+        2: ['var',
+            'axis',
+            'epics',
+            'encoder',
+            'trajectory',
+            'input',
+            'plc',
+            'homing',
+            'softlimits',
+            'monitoring']
+    }
 
-    @staticmethod
-    def get_schema(keys):
+    def get_schema(self, keys):
         _type = type(keys)
         if _type is list:
-            return {key: grandSchema[key] for key in keys}
+            return {key: self.grandSchema[key] for key in keys}
         elif _type is str:
-            return {keys: grandSchema[keys]}
+            return {keys: self.grandSchema[keys]}
         else:
             raise NotImplementedError(f'unsupported input type >>{_type}<<')
 
 
-axisSchema = {
-    'type': 'dict',
-    'required': True,
-    'schema': {
-        'id': {'required': True, 'type': 'integer', 'min': 1},
-        'type': {'type': 'integer', 'default': 'joint', 'coerce': lambda v: supportedAxisTypes[str(v).lower()]},
-        'mode': {'type': 'string', 'default': 'CSV', 'coerce': lambda v: v.upper()},
-        'parameters': {'type': 'string'}
-    }
-}
-
-epicsSchema = {
-    'type': 'dict',
-    'required': False,
-    'schema': {
-        'name': {'default': 'axis'},
-        'precision': {'type': 'integer', 'default': 3},
-        'unit': {'type': 'string', 'default': 'mm'},
-        'motorRecord': {
-            'type': 'dict',
-            'default': {'enable': True},
-            'schema': {
-                'enable': {'type': 'boolean', 'default': True},
-                'fieldInit': {'type': 'string'},
-                'description': {'type': 'string'}
+    filterSchema = {
+        'type': 'dict',
+        'schema': {
+            'velocity': {
+                'type': 'dict',
+                'schema': {
+                    'enable': {'type': 'boolean'},
+                    'size': {'type': 'integer'}
+                }
+            },
+            'trajectory': {
+                'type': 'dict',
+                'schema': {
+                    'enable': {'type': 'boolean'},
+                    'size': {'type': 'integer'}
+                }
             }
         }
     }
-}
 
-encoderSchema = {
-    'type': 'dict',
-    'required': True,
-    'schema': {
-        'numerator': {'type': 'float', 'default': 0},
-        'denominator': {'type': 'integer', 'default': 1, 'min': 1},
-        'type': {'type': 'integer', 'default': 0, 'allowed': [0, 1]},
-        'bits': {'type': 'integer', 'min': 0},
-        'absBits': {'type': 'integer', 'min': 0},
-        'absOffset': {'type': 'float', 'default': 0},
-        'position': {'type': 'string'},
-        'control': {'type': 'string'},
-        'status': {'type': 'string'},
-        'reset': {'type': 'integer', 'min': 0},
-        'warning': {'type': 'integer', 'min': 0},
-        'error': {'type': 'list', 'maxlength': 3},
-        'velocityFilterSize': {'type': 'integer', 'default': 100, 'min': 0},
-        'positionFilterSize': {'type': 'integer', 'default': 1, 'min': 0},
-        'positionFilterEnable': {'type': 'boolean', 'default': False},
-        'latch': {
-            'type': 'dict',
-            'schema': {
-                'position': {'type': 'float', 'default': 0},
-                'control': {'type': 'string'},
-                'status': {'type': 'string'},
+    axisSchema = {
+        'type': 'dict',
+        'required': True,
+        'schema': {
+            'id': {'required': True, 'type': 'integer', 'min': 1},
+            'type': {'type': 'integer', 'default': 'joint', 'coerce': lambda v: supportedAxisTypes[str(v).lower()]},
+            'mode': {'type': 'string', 'default': 'CSV', 'coerce': lambda v: v.upper()},
+            'parameters': {'type': 'string'},
+            'features': {'type': 'dict', 'schema': {
+                'disableOnReset': {'type': 'boolean'},
+                'AlarmAtHardLimits': {'type': 'boolean'},
+                'blockCom': {'type': 'boolean'},
+                'allowedFunctions': {'type': 'dict', 'schema': {
+                    'positioning': {'type': 'boolean'},
+                    'constantVelocity': {'type': 'boolean'},
+                    'homing': {'type': 'boolean'}
+                }}
+            }}
+        }
+    }
+
+    varSchema = {
+        'type': 'dict',
+        'required': False
+    }
+
+    epicsSchema = {
+        'type': 'dict',
+        'required': False,
+        'schema': {
+            'name': {'default': 'axis'},
+            'precision': {'type': 'integer', 'min': 0, 'default': 3},
+            'unit': {'type': 'string', 'default': 'mm'},
+            'motorRecord': {
+                'type': 'dict',
+                'default': {'enable': True},
+                'schema': {
+                    'enable': {'type': 'boolean', 'default': True},
+                    'fieldInit': {'type': 'string'},
+                    'description': {'type': 'string'}
+                }
             }
         }
     }
-}
 
-driveSchema = {
-    'type': 'dict',
-    'required': True,
-    'schema': {
-        'numerator': {'type': 'float', 'default': 0},
-        'denominator': {'type': 'integer', 'default': 1, 'min': 1},
-        'type': {'type': 'integer', 'default': 0, 'allowed': [0, 1]},
-        'reduceTorque': {'type': 'integer', 'min': 0},
-        'setpoint': {'type': 'string'},
-        'control': {'type': 'string'},
-        'status': {'type': 'string'},
-        'brake': {
-            'type': 'dict',
-            'schema': {
-                'openDelay': {'type': 'integer', 'default': 0, 'min': 0},
-                'closeAhead': {'type': 'integer', 'default': 0, 'min': 0}
+    driveSchema = {
+        'type': 'dict',
+        'required': True,
+        'schema': {
+            'numerator': {'type': 'float', 'default': 0},
+            'denominator': {'type': 'integer', 'default': 1, 'min': 1},
+            'type': {'type': 'integer', 'default': 0, 'allowed': [0, 1]},
+            'setpoint': {'type': 'string'},
+            'control': {'type': 'string'},
+            'status': {'type': 'string'},
+            'reduceTorqueEnable': {'type': 'boolean', 'dependencies': ['control', 'reduceTorque']},
+            'reduceTorque': {'type': 'integer', 'min': 0, 'dependencies': ['control', 'reduceTorqueEnable']},
+            'brake': {
+                'type': 'dict',
+                'schema': {
+                    'enable': {'type': 'boolean'},
+                    'output': {'type': 'string', 'dependencies': ['enable']},
+                    'openDelay': {'type': 'integer', 'default': 0, 'min': 0, 'dependencies': ['enable']},
+                    'closeAhead': {'type': 'integer', 'default': 0, 'min': 0, 'dependencies': ['enable']}
+                }
+            },
+            'warning': {'type': 'integer', 'min': 0, 'dependencies': ['status']},
+            'reset': {'type': 'integer', 'min': 0, 'dependencies': ['control']},
+            'error': {'type': 'list', 'maxlength': 3, 'schema': {'type': 'integer'}, 'dependencies': ['control']}
+        }
+    }
+
+    encoderSchema = {
+        'type': 'dict',
+        'required': True,
+        'schema': {
+            'source': {'type': 'integer', 'allowed': [0, 1]},
+            'numerator': {'type': 'float', 'default': 0.},
+            'denominator': {'type': 'integer', 'default': 1, 'min': 1},
+            'type': {'type': 'integer', 'default': 0, 'allowed': [0, 1]},
+            'mask': {'type': 'string'},
+            'bits': {'type': 'integer', 'min': 0, 'max': 64, 'default': 16},
+            'absBits': {'type': 'integer', 'min': 0, 'max': 64, 'default': 25},
+            'absOffset': {'type': 'float', 'default': 0.},
+            'position': {'required': True, 'type': 'string'},
+            'control': {'type': 'string'},
+            'status': {'type': 'string'},
+            'warning': {'type': 'integer', 'min': 0, 'dependencies': ['status']},
+            'reset': {'type': 'integer', 'min': 0, 'dependencies': ['control']},
+            'error': {'type': 'list', 'maxlength': 3, 'schema': {'type': 'integer'}, 'dependencies': ['control']},
+            'filter': filterSchema,
+            'latch': {
+                'type': 'dict',
+                'schema': {
+                    'position': {'type': 'float', 'default': 0.},
+                    'control': {'type': 'string'},
+                    'status': {'type': 'string'},
+                }
             }
-        },
-        'reset': {'type': 'integer', 'min': 0},
-        'warning': {'type': 'integer', 'min': 0},
-        'error': {'type': 'list', 'maxlength': 3}
+        }
     }
-}
 
-controllerSchema = {
-    'type': 'dict',
-    'required': False,
-    'default': {'controller': {'Kp': 1}},
-    'schema': {
-        'Kp': {'type': 'float', 'default': 1, 'min': 0},
-        'Ki': {'type': 'float', 'default': 0, 'min': 0},
-        'Kd': {'type': 'float', 'default': 0, 'min': 0},
-        'Kff': {'type': 'float', 'default': 1, 'min': 0},
+    controllerSchema = {
+        'type': 'dict',
+        'required': False,
+        'default': {'controller': {'Kp': 1}},
+        'schema': {
+            'Kp': {'type': 'float', 'default': 1., 'min': 0},
+            'Ki': {'type': 'float', 'default': 0., 'min': 0},
+            'Kd': {'type': 'float', 'default': 0., 'min': 0},
+            'Kff': {'type': 'float', 'default': 1., 'min': 0},
+            'limits': {
+                'type': 'dict',
+                'schema': {
+                    'minOutput': {'type': 'float'},
+                    'maxOutput': {'type': 'float'},
+                    'minIntegral': {'type': 'float'},
+                    'maxIntegral': {'type': 'float'}
+                }
+            }
+        }
     }
-}
 
-varSchema = {
-    'type': 'dict',
-    'required': False
-}
+    trajectorySchema = {
+        'type': 'dict',
+        'required': True,
+        'schema': {
+            'source': {'type': 'integer', 'default': 0, 'allowed': [0, 1]},
+            'axis': {
+                'type': 'dict',
+                'schema': {
+                    'velocity': {'required': True, 'type': 'float'},
+                    'acceleration': {'required': True, 'type': 'float'},
+                    'jerk': {'type': 'float'},
+                    'deceleration': {'type': 'float'},
+                    'emergencyDeceleration': {'type': 'float'}
+                }
+            },
+            'jog': {
+                'type': 'dict',
+                'schema': {
+                    'velocity': {'type': 'float'},
+                    'acceleration': {'type': 'float'},
+                }
+            },
+            'modulo': {
+                'type': 'dict',
+                'schema': {
+                    'range': {'type': 'float'},
+                    'type': {'type': 'integer', 'allowed': [0, 1, 2, 3]}
+                }
+            },
+        }
+    }
 
-grandSchema = {
-    'var': varSchema,
-    'axis': axisSchema,
-    'epics': epicsSchema,
-    'encoder': encoderSchema,
-    'drive': driveSchema,
-    'controller': controllerSchema,
-}
+    inputSchema = {
+        'type': 'dict',
+        'required': True,
+        'schema': {
+            'limit': {
+                'type': 'dict',
+                'required': True,
+                'schema': {
+                    'forward': {'required': True, 'type': 'string'},
+                    'forwardPolarity': {'type': 'integer', 'allowed': [0, 1]},
+                    'backward': {'required': True, 'type': 'string'},
+                    'backwardPolarity': {'type': 'integer', 'allowed': [0, 1]},
+                    'latchInput': {'type': 'boolean'}
+                }
+            },
+            'home': {'required': True, 'type': 'string'},
+            'homePolarity': {'type': 'integer', 'allowed': [0, 1]},
+            'interlock': {'required': True, 'type': 'string'},
+            'interlockPolarity': {'type': 'integer', 'allowed': [0, 1]},
+        }
+    }
+
+    plcSchema = {
+        'type': 'dict',
+        'required': False,
+        'schema': {
+            'enable': {'type': 'boolean'},
+            'externalCommands': {'type': 'boolean'},
+            'filter': filterSchema,
+        }
+    }
+
+    homingSchema = {
+        'type': 'dict',
+        'required': False,
+        'schema': {
+            'type': {'type': 'integer'},
+            'position': {'type': 'float', 'default': 0.0},
+            'postMoveEnable': {'type': 'boolean'},
+            'postMovePosition': {'type': 'float'},
+            'switchPolarity': {'type': 'integer', 'allowed': [0, 1]},
+            'latchCount': {'type': 'integer'},
+            'velocity': {
+                'type': 'dict',
+                'required': False,
+                'schema': {
+                    'to': {'type': 'float'},
+                    'from': {'type': 'float'},
+                }
+            },
+            'acceleration': {'type': 'float'},
+            'deceleration': {'type': 'float'},
+            'timeout': {'type': 'integer'},
+        }
+    }
+
+    softlimitsSchema = {
+        'type': 'dict',
+        'required': False,
+        'schema': {
+            'enable': {'type': 'boolean'},
+            'forwardEnable': {'type': 'boolean'},
+            'forward': {'type': 'float'},
+            'backwardEnable': {'type': 'boolean'},
+            'backward': {'type': 'float'},
+        }
+    }
+
+    monitoringSchema = {
+        'type': 'dict',
+        'required': False,
+        'schema': {
+            'lag': {
+                'type': 'dict',
+                'required': False,
+                'schema': {
+                    'enable': {'type': 'boolean'},
+                    'tolerance': {'type': 'float'},
+                    'time': {'type': 'integer'},
+                }
+            },
+            'target': {
+                'type': 'dict',
+                'required': False,
+                'schema': {
+                    'enable': {'type': 'boolean'},
+                    'tolerance': {'type': 'float'},
+                    'time': {'type': 'integer'},
+                }
+            },
+            'velocity': {
+                'type': 'dict',
+                'required': False,
+                'schema': {
+                    'enable': {'type': 'boolean'},
+                    'max': {'type': 'float'},
+                    'time': {
+                        'type': 'dict',
+                        'schema': {
+                            'trajectory': {'type': 'integer'},
+                            'drive': {'type': 'integer'},
+                        }
+                    },
+                }
+            },
+            'velocityDifference': {
+                'type': 'dict',
+                'required': False,
+                'schema': {
+                    'enable': {'type': 'boolean'},
+                    'max': {'type': 'float'},
+                    'time': {
+                        'type': 'dict',
+                        'schema': {
+                            'trajectory': {'type': 'integer'},
+                            'drive': {'type': 'integer'},
+                        }
+                    },
+                }
+            },
+        }
+    }
+
+    grandSchema = {
+        'var': varSchema,
+        'axis': axisSchema,
+        'epics': epicsSchema,
+        'encoder': encoderSchema,
+        'drive': driveSchema,
+        'controller': controllerSchema,
+        'trajectory': trajectorySchema,
+        'input': inputSchema,
+        'plc': plcSchema,
+        'homing': homingSchema,
+        'softlimits': softlimitsSchema,
+        'monitoring': monitoringSchema
+    }
