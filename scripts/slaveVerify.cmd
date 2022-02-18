eaa66c388b8e75bb0b31f0929b5a5295089bd39e
Niko Kivel <niko.kivel@psi.ch>
2022-02-18 11:13:21 +0100

    FW read out switchable in hardware script

--- scripts/slaveVerify.cmd
+++ scripts/slaveVerify.cmd
@@ -1,25 +1,29 @@
 #-d /**
 #-d   \brief  script for slave verification and optional reset
-#-d   \details will verify the slave identity and reset by writing to 0x1011
+#-d   \details will verify the slave identity and reset by writing to 0x1011, optionally read firmwre version from 0x100a
 #-d   \author Niko Kivel
 #-d   \file
 #-d   \note Macros
-#-d   \param RESET       macro to enable reset, defaults to 0 --> no reset
+#-d   \param RESET    macro to enable reset, defaults to 0 --> no reset
+#-d   \param READ_FW  macro to enable a firmware check, defaults to 0 --> no firmware read out
 #-d */
 
 #- verify slave identity
-ecmcEpicsEnvSetCalcTernary(ECMC_SLAVE_VERIFY, "${ECMC_DEV_MODE=0}==0", "","#- ")
-${ECMC_SLAVE_VERIFY}ecmcConfigOrDie "Cfg.EcSlaveVerify(0,${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID})"
+ecmcEpicsEnvSetCalcTernary(SLAVE_VERIFY, "${ECMC_DEV_MODE=0}==0","","#- ")
+${SLAVE_VERIFY}ecmcConfigOrDie "Cfg.EcSlaveVerify(0,${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID})"
 
 #- reset terminal
-ecmcEpicsEnvSetCalcTernary(ECMC_SLAVE_RESET, "${RESET=0}>0", "","#- ")
-${ECMC_SLAVE_VERIFY}${ECMC_SLAVE_RESET}ecmcConfigOrDie "Cfg.EcWriteSdo(${ECMC_EC_SLAVE_NUM},0x1011,0x1,1684107116,4)"
+ecmcEpicsEnvSetCalcTernary(SLAVE_RESET, "${RESET=0}>0","","#- ")
+${SLAVE_VERIFY}${SLAVE_RESET}ecmcConfigOrDie "Cfg.EcWriteSdo(${ECMC_EC_SLAVE_NUM},0x1011,0x1,1684107116,4)"
 
-#- Read firmware version
-${ECMC_SLAVE_VERIFY}${ECMC_SLAVE_RESET}ecmcConfig "EcReadSdo(${ECMC_EC_SLAVE_NUM},0x100a,0x0,2)"
-${ECMC_SLAVE_VERIFY}${ECMC_SLAVE_RESET}ecmcEpicsEnvSetCalc("ECMC_EC_SLAVE_FW", "${ECMC_CONFIG_RETURN_VAL=0}", "0x%04x")
-${ECMC_SLAVE_RESET}${ECMC_COMMENT=#} Firmware version: ${ECMC_EC_SLAVE_FW=0}
+#- read firmware version
+ecmcEpicsEnvSetCalcTernary(SLAVE_FW, "${READ_FW=0}>0","","#- ")
+${SLAVE_VERIFY}${SLAVE_FW}ecmcConfig "EcReadSdo(${ECMC_EC_SLAVE_NUM},0x100a,0x0,2)"
+${SLAVE_VERIFY}${SLAVE_FW}ecmcEpicsEnvSetCalc("ECMC_EC_SLAVE_FW", "${ECMC_CONFIG_RETURN_VAL=0}", "0x%04x")
+${SLAVE_VERIFY}${SLAVE_FW}Firmware version: ${ECMC_EC_SLAVE_FW=0}
 
-#- Reset
-epicsEnvUnset(ECMC_SLAVE_VERIFY)
-epicsEnvUnset(ECMC_COMMENT)
+#- reset envitronment
+epicsEnvUnset(SLAVE_VERIFY)
+epicsEnvUnset(SLAVE_RESET)
+epicsEnvUnset(SLAVE_FW)
+
