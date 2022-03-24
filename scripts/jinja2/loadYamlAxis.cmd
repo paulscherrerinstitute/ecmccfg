74a8956165c957b3ee6d4a3570bb9f3a84f78def
Niko Kivel <niko.kivel@psi.ch>
2022-03-24 13:20:06 +0100

    added DEV macro to loadYamlAxis.cmd

--- scripts/jinja2/loadYamlAxis.cmd
+++ scripts/jinja2/loadYamlAxis.cmd
@@ -4,10 +4,12 @@
 #-d   \author Niko Kivel
 #-d   \file
 #-d   \param FILE the yaml-file containing the PLC definition
+#-d   \param DEV  the device name (optional, defaults to ${IOC}
 #-d   \note Example calls:
 #-d   \note - call
 #-d   \code
 #-d     ${SCRIPTEXEC} "./loadYamlAxis.cmd" "FILE=./axis1.yaml"
+#-d     ${SCRIPTEXEC} "./loadYamlAxis.cmd" "FILE=./axis1.yaml, DEV=foobar"
 #-d   \endcode
 #-d */
 
@@ -18,9 +20,14 @@ on error halt
 #- MUST be in the same 'system'-context!!!
 system ". ${ECMC_CONFIG_ROOT}pythonVenv.sh -d ${ECMC_TMP_DIR}; python ${ECMC_CONFIG_ROOT}axisYamlJinja2.py -d ${ECMC_TMP_DIR} -T ${ECMC_CONFIG_ROOT} -D ${FILE} -o ${ECMC_TMP_DIR}${FILE}.axis"
 
+#- set device name, default to ${IOC}
+epicsEnvSet("ECMC_PREFIX"      "${DEV=${IOC}}:")
+
 #- check for ECMC-format PLC file and load the PLC
 ecmcFileExist("${ECMC_TMP_DIR}${FILE}.axis",1)
 ${SCRIPTEXEC} "${ECMC_TMP_DIR}${FILE}.axis"
 
 #- cleanup
 system "rm -rf ${ECMC_TMP_DIR}${FILE}.axis"
+#- reset PREFIX
+epicsEnvSet("ECMC_PREFIX"      "${SM_PREFIX}")
