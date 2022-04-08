ade70068af2610ac89cc9b595b54a72115c214b2
Niko Kivel <niko.kivel@psi.ch>
2022-04-08 15:33:11 +0200

    new encoder for EL5042, RLS LA11DCB2D0KB

---
+++ hardware/Encoders/ecmcEL5042-Encoder-ch12-RLS-LA11DCB2D0KB.cmd
@@ -0,0 +1,15 @@
+#-d /**
+#-d   \brief hardware script for ecmcEL5042-Encoder-ch1-RLS-LA11-24bit.cmd
+#-d   \details Parmetrization of EL5042  channel 1 for encoder RLS LA11
+#-d   \author Niko Kivel
+#-d   \file
+#-d */
+
+epicsEnvSet("ECMC_EC_SDO_INDEX"               "0x8008")     # CH1=0x8008. CH2=0x8018
+#- Call generic config file
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmcEL5042-Encoder-RLS-LA11DCB2D0KB.cmd
+
+epicsEnvSet("ECMC_EC_SDO_INDEX"               "0x8018")     # CH1=0x8008. CH2=0x8018
+#- Call generic config file
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmcEL5042-Encoder-RLS-LA11DCB2D0KB.cmd
+
