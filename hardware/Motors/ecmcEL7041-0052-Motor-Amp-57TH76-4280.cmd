03f6968f7db5c058b8308941f740c052db8a87f4
Anders Sandstrom <anders.sandstrom@esss.se>
2022-10-04 09:43:28 +0200

    Add amp motor config for EL7041-0052

---
+++ hardware/Motors/ecmcEL7041-0052-Motor-Amp-57TH76-4280.cmd
@@ -0,0 +1,24 @@
+#-d /**
+#-d   \brief hardware script for EL7041-0052-Motor-Amp-57TH76-4280
+#-d   \details Parmetrization of EL7041-0052 for motor Amp-57TH76-4280
+#-d   \author Anders Sandstrom
+#-d   \file
+#-d */
+
+#- Set max current to 2500 mA
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,2800,2)"
+
+#- Reduced current 500 mA
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,100,2)"
+
+#- Nominal voltage 48000 mV
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,48000,2)"
+
+#- Coil resistance 80 x 10⁻²Ω
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,113,2)"
+
+#- Motor steps per revoution 200
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x6,200,2)"
+
+#- Coil inductance 290 x 10⁻⁵H - NOT USED FOR EL7041
+#ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,290,2)"
