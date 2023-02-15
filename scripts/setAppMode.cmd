ee21f9e6a75e1ae33c151ce78cb2652cccea97b1
Anders Sandstrom <anders.sandstrom@esss.se>
2023-02-15 08:47:31 +0100

    Cleanup of config records

--- scripts/setAppMode.cmd
+++ scripts/setAppMode.cmd
@@ -15,6 +15,7 @@ ecmcEpicsEnvSetCalcTernary(ECMC_MASTER_CMD, "${ECMC_EC_MASTER_ID=0}>=0", "","#- 
 ${ECMC_MASTER_CMD} ecmcFileExist("ecmcEc.db",1,1)
 ${ECMC_MASTER_CMD} dbLoadRecords("ecmcEc.db","P=${ECMC_PREFIX},ECMC_EC_MP=${ECMC_EC_MP=m},PORT=${ECMC_ASYN_PORT},ADDR=0,TIMEOUT=1,MASTER_ID=${ECMC_EC_MASTER_ID},T_SMP_MS=${ECMC_SAMPLE_RATE_MS},TSE=${ECMC_TSE}")
 
+
 #- Load ecmc information into record
 #- Set -1 to mark last in chains
 epicsEnvSet(ECMC_AXES_CFG,"[${ECMC_AXES_CFG=""}-1]")
@@ -23,8 +24,8 @@ epicsEnvSet(ECMC_PLG_CFG,"[${ECMC_PLG_CFG=""}-1]")
 epicsEnvSet(ECMC_PLCS_CFG,"[${ECMC_PLCS_CFG=""}-1]")
 epicsEnvSet(ECMC_EC_CFG,"[${ECMC_EC_CFG=""}-1]")
 
-${ECMC_MASTER_CMD} ecmcFileExist("ecmcControllerConfigs.db",1,1)
-dbLoadRecords("ecmcControllerConfigs.db","P=${SM_PREFIX},AXES_IDS='${ECMC_AXES_CFG=""}',M_ID=${ECMC_EC_MASTER_ID} ,PLC_IDS='${ECMC_PLCS_CFG=""}',DS_IDS='${ECMC_DS_CFG=""}', PLG_IDS='${ECMC_PLG_CFG=""}', EC_IDS='${ECMC_EC_CFG=""}'")
+ecmcFileExist("ecmcMcuConfigs.db",1,1)
+dbLoadRecords("ecmcMcuConfigs.db","P=${ECMC_PREFIX},AXES_IDS='${ECMC_AXES_CFG=""}',PLC_IDS='${ECMC_PLCS_CFG=""}',DS_IDS='${ECMC_DS_CFG=""}', PLG_IDS='${ECMC_PLG_CFG=""}',M_ID=${ECMC_EC_MASTER_ID=-1} , EC_IDS='${ECMC_EC_CFG=""}'")
 
 epicsEnvUnset(ECMC_AXES_CFG)
 epicsEnvUnset(ECMC_DS_CFG)
