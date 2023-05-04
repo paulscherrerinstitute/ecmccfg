b86ad4a45911924adb9a6f25d7ab1abd4fe2f92a
Anders Sandström <anders.sandstroem@psi.ch>
2023-05-04 08:57:43 +0200

    Add support for EL7211-9014 (same as EL7211-0010)

---
+++ hardware/Beckhoff_7XXX/EL/ecmcEL7211-9014.cmd
@@ -0,0 +1,82 @@
+#-d /**
+#-d   \brief hardware script for EL7211-9014
+#-d   \details EL7211-0010 Servo terminal with OCT feedback and STO
+#-d   \author Anders Sandstroem
+#-d   \file
+#-d   \note SDOS
+#-d   \param [out] SDO 0x1011:01 --> 1684107116 \b reset
+#-d */
+
+#- ###########################################################
+#- ############ Information:
+#-  Description: EL7211-0010 Servo terminal with OCT feedback
+#-
+#-  Note: The sync mode can not be written to in newer firmwares:
+#-        (0x1C32,0x1,0x1C33,0x1). So then these lines below needs
+#-        to be removed or commented out.
+#-        The log will then show error like:
+#-           EtherCAT ERROR 0-46: SDO download 0x1C32:01 (2 bytes) aborted.
+#-           EtherCAT ERROR 0-46: SDO abort message 0x06090030: "Value range of parameter exceeded".
+#-           EtherCAT ERROR 0-46: SDO configuration failed.
+#-
+#- ###########################################################
+
+epicsEnvSet("ECMC_EC_HWTYPE"             "EL7211-0010")
+epicsEnvSet("ECMC_EC_VENDOR_ID"          "0x2")
+epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x1c2b3052")
+
+#- verify slave, including reset
+${SCRIPTEXEC} ${ecmccfg_DIR}slaveVerify.cmd "RESET=true"
+
+#- ############  read electronic nameplate
+ecmcConfigOrDie "Cfg.EcWriteSdo(${ECMC_EC_SLAVE_NUM},0x8008,0x1,1,1)"
+ecmcConfigOrDie "Cfg.EcWriteSdo(${ECMC_EC_SLAVE_NUM},0x8008,0x2,1,1)"
+ecmcConfigOrDie "Cfg.EcWriteSdo(${ECMC_EC_SLAVE_NUM},0x8008,0x3,1,1)"
+
+#- ############  set velocity loop parameters
+#- TODO: For some strange reason, those two paramters are read from the motor, but seem not to take effect
+#- Velocity loop integral time = 50
+ecmcConfigOrDie "Cfg.EcWriteSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x14,50,4)"
+#- Velocity loop proportianal gain = 50
+ecmcConfigOrDie "Cfg.EcWriteSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x15,50,4)"
+
+#- common PDOs for CSV
+${SCRIPTEXEC} ${ecmccfg_DIR}ecmcEX72XX_CSV.cmd
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a02,0x6010,0x07,32,velocityActual01)"
+
+#- ############ Distributed clocks config EL7211:
+ecmcEpicsEnvSetCalc("ECMC_TEMP_PERIOD_NANO_SECS",1000/${ECMC_EC_SAMPLE_RATE=1000}*1E6)
+ecmcEpicsEnvSetCalc("ECMC_TEMP_PERIOD_NANO_SECS_HALF",${ECMC_TEMP_PERIOD_NANO_SECS}/2)
+
+#- NOTE important the sync 1 period needs to be 0!
+ecmcConfigOrDie "Cfg.EcSlaveConfigDC(${ECMC_EC_SLAVE_NUM},0x700,${ECMC_TEMP_PERIOD_NANO_SECS},${ECMC_TEMP_PERIOD_NANO_SECS_HALF},0,0)"
+
+# Peak current (to be overwritten by motor config)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8011,0x11,1000,4)"
+
+ecmcEpicsEnvSetCalc("ECMC_TEMP_WHATCHDOG_1",1000/${ECMC_EC_SAMPLE_RATE=1000}*1000)
+ecmcEpicsEnvSetCalc("ECMC_TEMP_WHATCHDOG_2",${ECMC_TEMP_WHATCHDOG_1}*10)
+
+ecmcConfigOrDie "Cfg.EcSlaveConfigWatchDog(${ECMC_EC_SLAVE_NUM},${ECMC_TEMP_WHATCHDOG_1},${ECMC_TEMP_WHATCHDOG_2})"
+
+# NOTE: Sometimes the EL7211-0010 will not go to op with the following error in /var/log/messages:
+# Sep  8 09:54:21 mcag-epics4 kernel: EtherCAT ERROR 0-40: SDO download 0x1C32:01 (2 bytes) aborted.
+# Then if the below command is not executed the slave will go online abnd work. Could be related to firmware versions.. Also see below 0x1c33
+
+#- Sync mode
+#ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x1C32,0x1,3,2)"
+#- Cycle time
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x1C32,0x2,${ECMC_TEMP_PERIOD_NANO_SECS},4)"
+
+# NOTE: Sometimes the EL7211-0010 will not go to op with the following error in /var/log/messages:
+# Sep  8 09:54:21 mcag-epics4 kernel: EtherCAT ERROR 0-40: SDO download 0x1C33:01 (2 bytes) aborted.
+# Then if the below command is not executed the slave will go online abnd work. Could be related to firmware versions.. Also see above 0x1c32
+#- Sync mode
+#ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x1C33,0x1,3,2)"
+#- Cycle time
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x1C33,0x2,${ECMC_TEMP_PERIOD_NANO_SECS},4)"
+
+epicsEnvUnset(ECMC_TEMP_PERIOD_NANO_SECS)
+epicsEnvUnset(ECMC_TEMP_PERIOD_NANO_SECS_HALF)
+epicsEnvUnset(ECMC_TEMP_WHATCHDOG_1)
+epicsEnvUnset(ECMC_TEMP_WHATCHDOG_2)
