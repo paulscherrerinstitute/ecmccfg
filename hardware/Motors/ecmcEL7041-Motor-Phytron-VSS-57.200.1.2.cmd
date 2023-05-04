c7c009ee1f04bc33d2593b3445360917fd1551bd
Alvin Acerbo <alvin.acerbo@psi.ch>
2023-05-04 10:46:10 +0200

    fix step count to full step counts; fix mismatch in rated current between code and doc; update author

--- hardware/Motors/ecmcEL7041-Motor-Phytron-VSS-57.200.1.2.cmd
+++ hardware/Motors/ecmcEL7041-Motor-Phytron-VSS-57.200.1.2.cmd
@@ -1,7 +1,7 @@
 #-d /**
 #-d   \brief hardware script for EL7041-Motor-Phytron-VSS-57.200.1.2
-#-d   \details Parmetrization of EL7041 for motor Phytron VSS-57.200.1.2
-#-d   \author Anders Sandstroem
+#-d   \details Parameterization of EL7041 for motor Phytron VSS-57.200.1.2
+#-d   \author Alvin Acerbo
 #-d   \file
 #-d   \note Max current = 1.2 A/phase
 #-d   \note Inductance per phase = 11.6 mH
@@ -11,7 +11,7 @@
 #- Set max current to 1000mA (max current is 1.2A for this motor)
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,1000,2)"
 
-#- Reduced current 500mA
+#- Reduced current 800mA
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,800,2)"
 
 #- Nominal voltage 48.0V (unit 1mV) max 48V
@@ -20,8 +20,8 @@ ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,48000,2)"
 #- Coil resistance 2.0 Ohm (unit 10mOhm)
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,200,2)"
 
-#- Motor full steps count 32x200
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x6,6400,2)"
+#- Motor full steps count 200
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x6,200,2)"
 
 #- Coil inductance 11.6mH (unit 0.01mH) Not settable in EL7041
 #- ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,1160,2)"
