eb964607c3ea0b0c9da9652a63ff9bc09b770b4e
Anders Sandström <anders.sandstroem@psi.ch>
2023-05-12 14:59:00 +0200

    Add motor for used in PSI epics traing linear unit

---
+++ hardware/Motors/ecmcEP7047-1032-Motor-Micos-Epics-Training.cmd
@@ -0,0 +1,24 @@
+#-d /**
+#-d   \brief hardware script for EP7047-1032-Motor-Micos
+#-d   \details Parmetrization of EP7047-1032 for motor Micos
+#-d   \author Anders Sandström
+#-d   \file
+#-d   \note Max current = 1.5 A
+#-d   \note Inductance per phase = 2.9 mH
+#-d   \note Resistance = 1.7 Ohm
+#-d */
+
+#- Set max current to 500mA (actually max current is 1.5A for this motor)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,500,2)"
+
+#- Reduced current 100mA
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,250,2)"
+
+#- Nominal voltage 48V (unit 10mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,4800,2)"
+
+#- Coil resistance 1.7 Ohm (unit 10mOhm)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,170,2)"
+
+#- Coil inductance 2.9mH (unit 0.01mH)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,290,2)"
