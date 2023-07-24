392fc1ae69a2bec46314fbc2c0d9c6c88541786e
Carlos Doro Neto <carlos.neto@ess.eu>
2023-07-24 16:12:38 +0200

    Motor review: Nanotec ST2818S1006 (bipolar wiring)

--- hardware/Motors/ecmcEL7037-Motor-Nanotec-ST2818S1006-bipolar.cmd
+++ hardware/Motors/ecmcEL7037-Motor-Nanotec-ST2818S1006-bipolar.cmd
@@ -1,25 +1,11 @@
 #-d /**
 #-d   \brief hardware script for EL7037-Motor-Nanotec-ST2818S1006 (bipolar series wiring)
 #-d   \details Parmetrization of EL7037 for motor Nanotec ST2818S1006 (bipolar series wiring)
-#-d   \author Javier Cereijo Garcia
 #-d   \file
 #-d */
 
-#- Set max current to 670mA
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,670,2)"
+#- Load common config from EL7047
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmcEL7047-Motor-Nanotec-ST2818S1006-bipolar.cmd
 
-#- Reduced current 200mA
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,200,2)"
-
-#- Nominal voltage up to 48V (unit 10mV)
-#- Torque curve varies with voltage, check manufacture website
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,4800,2)"
-
-#- Coil resistance 5.6 Ohm (unit 10mOhm)
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,560,2)"
-
-#- Motor full steps count 200
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x6,200,2)"
-
-#- Coil inductance 4mH (unit 0.01mH)
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,400,2)"
+#- Nominal voltage 24V (unit 10mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,2400,2)"
---
+++ hardware/Motors/ecmcEL7047-Motor-Nanotec-ST2818S1006-bipolar.cmd
@@ -0,0 +1,16 @@
+#-d /**
+#-d   \brief hardware script for EL7047-Motor-Nanotec-ST2818S1006 (bipolar series wiring)
+#-d   \details Parmetrization of EL7047 for motor Nanotec ST2818S1006 (bipolar series wiring)
+#-d   \author Javier Cereijo Garcia
+#-d   \author Carlos Doro Neto
+#-d   \file
+#-d */
+
+#- Load common config from EL7041
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmcEL7041-Motor-Nanotec-ST2818S1006-bipolar.cmd
+
+#- Nominal voltage up to 48V (unit 10mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,4800,2)"
+
+#- Coil inductance 4mH (unit 0.01mH)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,400,2)"
