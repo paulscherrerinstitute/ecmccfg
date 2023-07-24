6e447ca681fdf74e987fe86c6881a17053237b35
Carlos Doro Neto <carlos.neto@ess.eu>
2023-07-24 15:48:22 +0200

    Motor review: Nanotec ST2818M1006 (bipolar wiring)

---
+++ hardware/Motors/ecmcEL7047-Motor-Nanotec-ST2818M1006-bipolar.cmd
@@ -0,0 +1,16 @@
+#-d /**
+#-d   \brief hardware script for EL7047-Motor-Nanotec-ST2818M1006 (bipolar series wiring)
+#-d   \details Parmetrization of EL7047 for motor Nanotec ST2818M1006 (bipolar series wiring)
+#-d   \author Julen Etxeberria Malkorra
+#-d   \author Carlos Doro Neto
+#-d   \file
+#-d */
+
+#- Load common config from EL7041
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmcEL7041-Motor-Nanotec-ST2818M1006-bipolar.cmd
+
+#- Nominal voltage up to 48V (unit 10mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,4800,2)"
+
+#- Coil inductance 4.8mH (unit 0.01mH)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,480,2)"
