ff3e67d3ad11fcf7d3fd2bc88a8df654965370fd
Carlos Doro Neto <carlos.neto@ess.eu>
2023-07-26 16:52:56 +0200

    Motor review: Nanotec ST5918L1008 (unipolar)

---
+++ hardware/Motors/ecmcEL7041-Motor-Nanotec-ST5918L1008-unipolar.cmd
@@ -0,0 +1,22 @@
+#-d /**
+#-d   \brief hardware script for EL7041-Motor-Nanotec-ST5918L1008 (unipolar wiring)
+#-d   \details Parmetrization of EL7041 for motor Nanotec ST5918L1008 (unipolar wiring)
+#-d   \author Anders Sandstroem
+#-d   \author Carlos Doro Neto
+#-d   \file
+#-d */
+
+#- Set max current to 1A (unit 1mA)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,1000,2)"
+
+#- Reduced current 200mA (unit 1mA)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,200,2)"
+
+#- Nominal voltage 48V (unit 1mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,48000,2)"
+
+#- Coil resistance 8.8 Ohm (unit 10mOhm)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,880,2)"
+
+#- Motor full steps count 200
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x6,200,2)"
