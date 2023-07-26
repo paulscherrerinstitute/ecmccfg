2255c12b893117910c8ca6ca9966afc7250efa6f
Carlos Doro Neto <carlos.neto@ess.eu>
2023-07-26 17:05:20 +0200

    Motor review: Nanotec ST5918L1008 (parallel)

---
+++ hardware/Motors/ecmcEL7037-Motor-Nanotec-ST5918L1008-parallel.cmd
@@ -0,0 +1,11 @@
+#-d /**
+#-d   \brief hardware script for EL7037-Motor-Nanotec-ST5918L1008 (parallel wiring)
+#-d   \details Parmetrization of EL7037 for motor Nanotec ST5918L1008 (parallel wiring)
+#-d   \file
+#-d */
+
+#- Load common config from EL7047
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmcEL7047-Motor-Nanotec-ST5918L1008-parallel.cmd
+
+#- Nominal voltage 24V (unit 10mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,2400,2)"
