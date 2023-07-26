d02ff75e3452ec87421bc117d548a1bf160ff0b9
Carlos Doro Neto <carlos.neto@ess.eu>
2023-07-26 17:56:36 +0200

    Motor review: Phytron VSS 52.200.2.5
    Increases max current to 2.5A from 1.2A

--- hardware/Motors/ecmcEL7047-Motor-Phytron-VSS-52.200.2.5.cmd
+++ hardware/Motors/ecmcEL7047-Motor-Phytron-VSS-52.200.2.5.cmd
@@ -5,21 +5,11 @@
 #-d   \file
 #-d */
 
-#- Set max current to 2500mA (actually max current is 2.5A for this motor)
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,2500,2)"
+#- Load common config from EL7041
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmcEL7041-Motor-Phytron-VSS-52.200.2.5.cmd
 
-#- Reduced current 500mA
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,500,2)"
-
-#- Nominal voltage 48V (unit 1mV)
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,48000,2)"
-
-#- Coil resistance 0.6 Ohm (unit 10mOhm)
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,60,2)"
-
-#- Motor full steps count 200
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x6,200,2)"
+#- Nominal voltage 48V (unit 10mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,4800,2)"
 
 #- Coil inductance 1.6mH (unit 0.01mH)
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,160,2)"
-
