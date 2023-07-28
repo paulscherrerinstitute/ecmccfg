a05e187d30128a5d9ecbea5f71325888d702d33d
Carlos Doro Neto <carlos.neto@ess.eu>
2023-07-28 11:52:11 +0200

    Motor review: Leadshine 57HS09 (unipolar)

---
+++ hardware/Motors/ecmcEL7041-Motor-Leadshine-57HS09-unipolar.cmd
@@ -0,0 +1,20 @@
+#-d /**
+#-d   \brief hardware script for EL7041-Motor-Leadshine-57HS09 (unipolar wiring)
+#-d   \details Parmetrization of EL7041 for motor Nanotec Leadshine Tecnology mod. 57HS09 (unipolar wiring)
+#-d   \file
+#-d */
+
+#- Set max current to 2.8A (unit 1mA)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,2800,2)"
+
+#- Reduced current 500mA (unit 1mA)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,500,2)"
+
+#- Nominal voltage 48V (unit 1mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,48000,2)"
+
+#- Coil resistance 0.8 Ohm (unit 10mOhm)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,80,2)"
+
+#- Motor full steps count 200
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x6,200,2)"
---
+++ hardware/Motors/ecmcEL7047-Motor-Leadshine-57HS09-unipolar.cmd
@@ -0,0 +1,14 @@
+#-d /**
+#-d   \brief hardware script for EL7047-Motor-Leadshine-57HS09 (unipolar wiring)
+#-d   \details Parmetrization of EL7047 for motor Nanotec Leadshine Tecnology mod. 57HS09 (unipolar wiring)
+#-d   \file
+#-d */
+
+#- Load common config from EL7041
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmcEL7041-Motor-Leadshine-57HS09-unipolar.cmd
+
+#- Nominal voltage 48V (unit 10mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,4800,2)"
+
+#- Coil inductance 1.2mH (unit 0.01mH)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,120,2)"
