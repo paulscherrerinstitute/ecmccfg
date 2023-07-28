4272faf6c740b74926d586a456dd84f6f9aaf2ba
Carlos Doro Neto <carlos.neto@ess.eu>
2023-07-28 11:26:36 +0200

    Fix voltage for Oriental PK296DB

--- hardware/Motors/ecmcEL7041-Motor-OrientalMotor-PK296DB.cmd
+++ hardware/Motors/ecmcEL7041-Motor-OrientalMotor-PK296DB.cmd
@@ -14,8 +14,8 @@ ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,4000,2)"
 #- Reduced current 1000mA
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,1000,2)"
 
-#- Nominal voltage 48V (unit 10mV)
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,4800,2)"
+#- Nominal voltage 48V (unit 1mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,48000,2)"
 
 #- Coil resistance 0.24 Ohm (unit 10mOhm)
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,24,2)"
