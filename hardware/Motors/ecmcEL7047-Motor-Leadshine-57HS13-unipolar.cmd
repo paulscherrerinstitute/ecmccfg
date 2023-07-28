c7c0f688e66681618586f181f4b567ad18dc3fc3
Carlos Doro Neto <carlos.neto@ess.eu>
2023-07-28 12:12:32 +0200

    Add Leadshine 57HS13 (not tested)

---
+++ hardware/Motors/ecmcEL7047-Motor-Leadshine-57HS13-unipolar.cmd
@@ -0,0 +1,14 @@
+#-d /**
+#-d   \brief hardware script for EL7047-Motor-Leadshine-57HS13 (unipolar wiring)
+#-d   \details Parmetrization of EL7047 for motor Nanotec Leadshine Tecnology mod. 57HS13 (unipolar wiring)
+#-d   \file
+#-d */
+
+#- Load common config from EL7041
+${SCRIPTEXEC} ${ECMC_CONFIG_ROOT}ecmcEL7041-Motor-Leadshine-57HS13-unipolar.cmd
+
+#- Nominal voltage 68V (unit 10mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,6800,2)"
+
+#- Coil inductance 2.1mH (unit 0.01mH)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,210,2)"
