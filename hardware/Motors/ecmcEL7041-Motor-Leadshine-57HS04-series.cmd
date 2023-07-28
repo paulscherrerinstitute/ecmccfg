c9c3b6982d90feeb4456881117bf45264244f193
Carlos Doro Neto <carlos.neto@ess.eu>
2023-07-28 12:02:21 +0200

    Add Leadshine 57HS04 (not tested)

---
+++ hardware/Motors/ecmcEL7041-Motor-Leadshine-57HS04-series.cmd
@@ -0,0 +1,20 @@
+#-d /**
+#-d   \brief hardware script for EL7041-Motor-Leadshine-57HS04 (series wiring)
+#-d   \details Parmetrization of EL7041 for motor Nanotec Leadshine Tecnology mod. 57HS04 (series wiring)
+#-d   \file
+#-d */
+
+#- Set max current to 2A (unit 1mA)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,2000,2)"
+
+#- Reduced current 500mA (unit 1mA)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,500,2)"
+
+#- Nominal voltage 36V (unit 1mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,36000,2)"
+
+#- Coil resistance 1.4 Ohm (unit 10mOhm)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,140,2)"
+
+#- Motor full steps count 200
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x6,200,2)"
