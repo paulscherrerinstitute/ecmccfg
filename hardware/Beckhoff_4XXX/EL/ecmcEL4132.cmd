88d0e032ae8d1e5424527a45310d13a2d989a727
Anders Sandström <anders.sandstroem@psi.ch>
2023-11-03 16:02:33 +0100

    Add hardware support: EL2828, EL4112-0010, EL4132

---
+++ hardware/Beckhoff_4XXX/EL/ecmcEL4132.cmd
@@ -0,0 +1,25 @@
+#-d /**
+#-d   \brief hardware script for EL4132
+#-d   \details 2 channel analog output module +-10V 16 bit
+#-d   \author Anders Sandstroem
+#-d   \file
+#-d   \note SDOS
+#-d   \param [out] SDO 0x1011:01 --> 1684107116 \b reset
+#-d
+#-d */
+
+epicsEnvSet("ECMC_EC_HWTYPE"             "EL4132")
+epicsEnvSet("ECMC_EC_VENDOR_ID"          "0x2")
+epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x10243052")
+
+#- verify slave, including reset
+ecmcFileExist(${ecmccfg_DIR}slaveVerify.cmd,1)
+${SCRIPTEXEC} ${ecmccfg_DIR}slaveVerify.cmd "RESET=true"
+
+ecmcFileExist(${ecmccfg_DIR}ecmcAnalogOutput_16bit.cmd,1)
+
+#- ############ Config PDOS: Channel 1
+${SCRIPTEXEC} ${ecmccfg_DIR}ecmcAnalogOutput_16bit.cmd "CH_ID=01,ECMC_PDO=0x1600,ECMC_ENTRY=0x7000,ECMC_ENTRY_OFFSET=0x1"
+
+#- ############ Config PDOS: Channel 2
+${SCRIPTEXEC} ${ecmccfg_DIR}ecmcAnalogOutput_16bit.cmd "CH_ID=02,ECMC_PDO=0x1601,ECMC_ENTRY=0x7010,ECMC_ENTRY_OFFSET=0x1"
