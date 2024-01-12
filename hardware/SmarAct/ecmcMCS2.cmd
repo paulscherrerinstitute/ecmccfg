de245a082ac6a4fda9ddc0ee2d3613d5d7efc998
Anders Sandström <anders.sandstroem@psi.ch>
2024-01-12 15:45:09 +0100

    Smaract: Correct offset to home exe bit. Homing is executing.

--- hardware/SmarAct/ecmcMCS2.cmd
+++ hardware/SmarAct/ecmcMCS2.cmd
@@ -45,7 +45,7 @@ ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1601,0x6060,0x00,16,mode${ECMC_MCS2_CHID})"
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1601,0x607a,0x00,32,1,positionSetpoint${ECMC_MCS2_CHID})"
 #- Add bits for homing: driveControl01.4
-ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,5,1,B1,driveHomeCmd${ECMC_MCS2_CHID})"
+ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,4,1,B1,driveHomeCmd${ECMC_MCS2_CHID})"
 #- Add bits for calibration: driveControl01.??
 #ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,???,1,B1,driveCalibrationCmd${ECMC_MCS2_CHID})"
 
@@ -56,7 +56,7 @@ ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1641,0x6860,0x00,16,mode${ECMC_MCS2_CHID})"
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1641,0x687a,0x00,32,1,positionSetpoint${ECMC_MCS2_CHID})"
 #- Add bits for homing: driveControl02.4
-ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,5,1,B1,driveHomeCmd${ECMC_MCS2_CHID})"
+ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,4,1,B1,driveHomeCmd${ECMC_MCS2_CHID})"
 #- Add bits for calibration: driveControl02.??
 #ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,???,1,B1,driveCalibrationCmd${ECMC_MCS2_CHID})"
 
@@ -67,7 +67,7 @@ ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1681,0x7060,0x00,16,mode${ECMC_MCS2_CHID})"
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1681,0x707a,0x00,32,1,positionSetpoint${ECMC_MCS2_CHID})"
 #- Add bits for homing: driveControl03.4
-ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,5,1,B1,driveHomeCmd${ECMC_MCS2_CHID})"
+ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,4,1,B1,driveHomeCmd${ECMC_MCS2_CHID})"
 #- Add bits for calibration: driveControl03.??
 #ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,???,1,B1,driveCalibrationCmd${ECMC_MCS2_CHID})"
 
@@ -79,7 +79,7 @@ ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a01,0x6041,0x00,16,driveStatus${ECMC_MCS2_CHID})"
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a01,0x6064,0x00,32,1,positionActual${ECMC_MCS2_CHID})"
 #- Add bits for homing: driveStatus01.10 (1 byte 3 bits)
-ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveStatus${ECMC_MCS2_CHID},1,3,2,B1,driveHomeDone${ECMC_MCS2_CHID})"
+ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveStatus${ECMC_MCS2_CHID},1,2,2,B1,driveHomeDone${ECMC_MCS2_CHID})"
 #- Check final control word (after applying data items)
 ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,0,2,U16,driveControl${ECMC_MCS2_CHID}_RB)"
 
@@ -90,7 +90,7 @@ ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a41,0x6841,0x00,16,driveStatus${ECMC_MCS2_CHID})"
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a41,0x6864,0x00,32,1,positionActual${ECMC_MCS2_CHID})"
 #- Add bits for homing: driveStatus02.10 (1 byte 3 bits)
-ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveStatus${ECMC_MCS2_CHID},1,3,2,B1,driveHomeDone${ECMC_MCS2_CHID})"
+ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveStatus${ECMC_MCS2_CHID},1,2,2,B1,driveHomeDone${ECMC_MCS2_CHID})"
 #- Check final control word (after applying data items)
 ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,0,2,U16,driveControl${ECMC_MCS2_CHID}_RB)"
 
@@ -101,7 +101,7 @@ ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a81,0x7041,0x00,16,driveStatus${ECMC_MCS2_CHID})"
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a81,0x7064,0x00,32,1,positionActual${ECMC_MCS2_CHID})"
 #- Add bits for homing: driveStatus03.10 (1 byte 3 bits)
-ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveStatus${ECMC_MCS2_CHID},1,3,2,B1,driveHomeDone${ECMC_MCS2_CHID})"
+ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveStatus${ECMC_MCS2_CHID},1,2,2,B1,driveHomeDone${ECMC_MCS2_CHID})"
 #- Check final control word (after applying data items)
 ecmcConfigOrDie "Cfg.EcAddDataDT(ec${ECMC_EC_MASTER_ID=0}.s${ECMC_EC_SLAVE_NUM}.driveControl${ECMC_MCS2_CHID},0,0,2,U16,driveControl${ECMC_MCS2_CHID}_RB)"
 
