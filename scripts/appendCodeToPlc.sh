b25382eedd8afe0844b26b75d5918745719cf217
Anders Sandström <anders.sandstroem@psi.ch>
2024-01-23 11:50:34 +0100

    WIP

--- scripts/appendCodeToPlc.sh
+++ scripts/appendCodeToPlc.sh
@@ -3,5 +3,5 @@
 # File that writes a line of code to an output file and exchanges "€" to "'"
 CODELINE=$1
 OUTPUT_FILE=$2
-CHR_TO_REPLACE=$3
-echo "$CODELINE" | sed "s/$CHR_TO_REPLACE/\'/g" >> $OUTPUT_FILE
+STR_TO_REPLACE=$3
+echo "$CODELINE" | sed "s/$STR_TO_REPLACE/\'/g" >> $OUTPUT_FILE
