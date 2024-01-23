749cf511e24d9fc1b9fc21b1edff0b296d9bca52
Anders Sandström <anders.sandstroem@psi.ch>
2024-01-23 11:59:54 +0100

    Fix macros

--- scripts/appendCodeToPlc.sh
+++ scripts/appendCodeToPlc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# File that writes a line of code to an output file and exchanges "€" to "'"
+# File that writes a line of code to an output file and exchanges "€" to "'" "£" to "$"
 CODELINE=$1
 OUTPUT_FILE=$2
 STR_TO_REPLACE=$3
-echo "$CODELINE" | sed "s/$STR_TO_REPLACE/\'/g" >> $OUTPUT_FILE
+DOLLAR_TO_REPLACE=$4
+echo "$CODELINE" | sed "s/$STR_TO_REPLACE/\'/g" | sed "s/$DOLLAR_TO_REPLACE/$/g" >> $OUTPUT_FILE
