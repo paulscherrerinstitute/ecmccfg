8900c756aec73f9c36305f1dcb9bd9274d7d56ea
Carlos Doro Neto <carlos.neto@ess.eu>
2024-02-15 17:29:12 +0100

    Cleanup EL70x1 and EL70x7
    Remove "speed range" (default value already is 2000 steps/s)
    Remove "invert motor polarity" (default value already is 0)
    Remove "feedback type" (default value already is 1)
    Use ecmcEX70XX.cmd for EL7041-0052 and EL7041-1000 (also fixes a missing close parenthesis)

--- hardware/Beckhoff_7XXX/EL/ecmcEL7047.cmd
+++ hardware/Beckhoff_7XXX/EL/ecmcEL7047.cmd
@@ -17,14 +17,5 @@ ${SCRIPTEXEC} ${ecmccfg_DIR}slaveVerify.cmd "RESET=true"
 #- common PDOs for status and control
 ${SCRIPTEXEC} ${ecmccfg_DIR}ecmcEX70XX.cmd
 
-#- Max full step freq = 2000Hz (setting is 1)
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8012,0x5,1,1)"
-
 #- Error on loss of steps = 1
 ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8012,0xA,1,1)"
-
-#- Feedback type = Internal counter = 1 (for encoder on terminal set to 0)
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8012,0x8,1,1)"
-
-#- Invert motor polarity = 0
-ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8012,0x9,0,1)"
