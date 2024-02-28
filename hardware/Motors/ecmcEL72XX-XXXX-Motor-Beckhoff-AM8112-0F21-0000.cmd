1de07421c5aa49c7f2979797963c8fc6af138a87
Niko Kivel <niko.kivel@lightsource.ca>
2024-02-28 14:04:45 -0600

    AM8112-0F21

---
+++ hardware/Motors/ecmcEL72XX-XXXX-Motor-Beckhoff-AM8112-0F21-0000.cmd
@@ -0,0 +1,48 @@
+
+############################################################
+############# Parmetrization of EL72XX-XXXX for motor AM8113-0F21-0000
+#- Motor WITH brake
+#-
+#- Note: For important parameters see TwinCAT CoE startup list for the 
+#-       motor terminal configuration.
+#-
+#- Macros (optional):
+#-d   \param I_MAX_MA : Maximum current in mA (defaults to 4700 mA)
+#-d   \param I_RUN_MA : Running current in mA (defaults to 4500 mA)
+#-d   \param V_NOM_MV : Nominal voltage in mV (defaults to 33900 mV)
+
+#- Nominal voltage
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x19,${V_NOM_MV=33900},4)"
+#- Motor max current
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8011,0x11,${I_MAX_MA=4700},4)"
+#- Motor rated current
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8011,0x12,${I_RUN_MA=4500},4)"
+#- Motor pole pairs
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8011,0x13,3,1)"
+#- Torque contstant
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8011,0x16,80,4)"
+#- Winding inductance
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8011,0x19,7,2)"
+#- Commutation offset
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8011,0x15,270,2)"
+#- Motor thermal time constant
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8011,0x2d,5400,2)"
+#- Motor speed limitation
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8011,0x1b,3885,4)"
+#- Current loop proportianal gain
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x13,106,2)"
+#- Current loop integral time
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x12,5,2)"
+#- Velocity loop proportianal gain
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x15,150,4)"
+#- Velocity loop integral time
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x14,50,4)"
+
+#- ###### Differs with brake:
+#- Rotor moment of inertia
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8011,0x18,75,4)"
+#- Application delay
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8012,0x12,14,2)"
+#- Release delay
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8012,0x11,8,2)"
+
