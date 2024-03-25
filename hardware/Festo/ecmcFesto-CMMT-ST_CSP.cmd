baa788afae9b503224fcb365a6257af28fb4ab6b
Anders Sandström <anders.sandstroem@psi.ch>
2024-03-25 16:33:28 +0100

    CMMT-ST: WIP

--- hardware/Festo/ecmcFesto-CMMT-ST_CSP.cmd
+++ hardware/Festo/ecmcFesto-CMMT-ST_CSP.cmd
@@ -8,9 +8,10 @@
 #-d */
 
 
-epicsEnvSet("ECMC_EC_HWTYPE"             "Festo-CMMT-ST")
+epicsEnvSet("ECMC_EC_HWTYPE"             "Festo-CMMT-ST_CSP")
 epicsEnvSet("ECMC_EC_VENDOR_ID"          "0x0000001d")
 epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x007b5a25")
+epicsEnvSet("ECMC_EC_COMP_TYPE"          "Festo-CMMT-ST")
 
 #- verify slave
 ecmcFileExist(${ecmccfg_DIR}slaveVerify.cmd,1)
@@ -30,7 +31,9 @@ ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x6041,0x0,16,driveStatus01)"
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x6061,0x0,8,1,modeStatus01)"
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x6064,0x0,32,1,positionActual01)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x60E4,0x1,32,1,positionActual02)"
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x606C,0x0,32,1,velocityActual01)"
+ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x60E5,0x1,32,1,velocityActual02)"
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x6077,0x0,16,1,torqueActual01)"
 
 # Default to CSV
