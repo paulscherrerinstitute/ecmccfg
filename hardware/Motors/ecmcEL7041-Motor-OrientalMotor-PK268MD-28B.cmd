61994bd881d248f3e154244878b89e6d39065851
Anders Sandström <anders.sandstroem@psi.ch>
2024-04-03 15:43:07 +0200

    Add enc and motor cfgs

---
+++ hardware/Motors/ecmcEL7041-Motor-OrientalMotor-PK268MD-28B.cmd
@@ -0,0 +1,28 @@
+#-d /**
+#-d   \brief hardware script for EL7041-Motor-OrientalMotor-PK268MD
+#-d   \details Parmetrization of EL7041 for motor Oriental Motor PK268MD
+#-d   \author Anders Sandstroem
+#-d   \file
+#-d   \note Max current = 2.8 A/phase
+#-d   \note Inductance per phase = 5.6 mH
+#-d   \note Resistance = 1.23 Ohm (Measured: 5.6 Ohm)
+#-d */
+
+#- Set max current to 2000mA (max current is 2.8A for this motor) (unit 1 mA)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x1,2000,2)"
+
+#- Reduced current 600mA (unit 1 mA)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x2,600,2)"
+
+#- Nominal voltage 48V (unit 1mV)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x3,48000,2)"
+
+#- Coil resistance 1.23 Ohm measured 3.2+0.4 Ohm (unit 10mOhm)
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,123,2)"
+#ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x4,560,2)"
+
+#- Motor full steps count 200
+ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0x6,200,2)"
+
+#- Coil inductance 5.6mH (unit 0.01mH) Not settable in EL7041
+#- ecmcConfigOrDie "Cfg.EcAddSdo(${ECMC_EC_SLAVE_NUM},0x8010,0xA,560,2)"
