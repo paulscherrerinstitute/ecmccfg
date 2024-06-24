03406f4795cbdfa8c163ea8cf8861ad67856aacc
Anders Sandström <anders.sandstroem@psi.ch>
2024-06-24 15:32:03 +0200

    caqtdm: Add buttons for next and prev axis

--- qt/ecmcOpenPrevAxis.sh
+++ qt/ecmcOpenPrevAxis.sh
@@ -1,5 +1,6 @@
 PREFIX=$1
-AX_ID=$( caget -noname -nostat -nounit -int $PREFIX:MCU-Cfg-AX-FrstObjId | tr -d '"')
+THIS_AX=$2  
+AX_ID=$( caget -noname -nostat -nounit -int $PREFIX:MCU-Cfg-AX$THIS_AX-PrvObjId | tr -d '"')
 echo "AX_ID=$AX_ID"
 AX_PREFIX=$( caget -noname -nostat -nounit $PREFIX:MCU-Cfg-AX$AX_ID-Pfx | tr -d '"' | tr -d ":")
 echo "AX_PREFIX=$AX_PREFIX"
@@ -8,6 +9,3 @@ echo "AX_NAME=$AX_NAME"
 MACROS="SYS=$PREFIX,IOC=$AX_PREFIX,Axis=$AX_NAME,AX_ID=$AX_ID"
 echo "MACROS=$MACROS"
 caqtdm -macro $MACROS ecmcAxisExpert_v2.ui
-
-
-
