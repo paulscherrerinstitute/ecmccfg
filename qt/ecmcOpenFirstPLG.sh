0556aa5007cd7cf055b776ed49eebdb040b9eb2c
Anders Sandström <anders.sandstroem@psi.ch>
2024-07-02 14:52:32 +0200

    Work related to panels

---
+++ qt/ecmcOpenFirstPLG.sh
@@ -0,0 +1,8 @@
+PREFIX=$1
+ID_1=$( caget -noname -nostat -nounit -int $PREFIX:MCU-Cfg-PLG-FrstObjId | tr -d '"')
+ID_2=$(printf  "%02d" $ID_1)
+echo "ID_1=$ID_1"
+echo "ID_2=$ID_2"
+MACROS="SYS=$PREFIX,IOC=$PREFIX,ID_1=$ID_1,ID_2=$ID_2"
+echo "MACROS=$MACROS"
+caqtdm -macro $MACROS ecmcPLGxx.ui
