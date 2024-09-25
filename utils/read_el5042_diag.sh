e6084a2e416435448bbb9db20d09b3c6344f212e
Anders Sandström <anders.sandstroem@psi.ch>
2024-09-25 14:14:23 +0200

    Fix stat read script for EL5042

--- utils/read_el5042_diag.sh
+++ utils/read_el5042_diag.sh
@@ -1,28 +1,29 @@
 MID=$1
 SID=$2
-CH_ID=${$3:-0}
+CH_ID=$3
 
+echo "0xA0${CH_ID}0"
 echo ""
 echo "#########################################################"
 echo "Reading EL5042 ch $CH_ID status at master id $MID and slave id $SID:"
 echo ""
 echo "Power supply present:"
-ethercat upload -m $MID -p $SID --type uint8  0xA0$CH_ID0 0x1
+ethercat upload -m $MID -p $SID --type uint8  0xA0${CH_ID}8 0x1
 
 echo "Error:"
-ethercat upload -m $MID -p $SID --type uint8  0xA0$CH_ID0 0x2
+ethercat upload -m $MID -p $SID --type uint8  0xA0${CH_ID}8 0x2
 
 echo "SDC Error:"
-ethercat upload -m $MID -p $SID --type uint8  0xA0$CH_ID0 0x3
+ethercat upload -m $MID -p $SID --type uint8  0xA0${CH_ID}8 0x3
 
 echo "WD Error:"
-ethercat upload -m $MID -p $SID --type uint8  0xA0$CH_ID0 0x4
+ethercat upload -m $MID -p $SID --type uint8  0xA0${CH_ID}8 0x4
 
 echo "Data valid:"
-ethercat upload -m $MID -p $SID --type uint8  0xA0$CH_ID0 0x5
+ethercat upload -m $MID -p $SID --type uint8  0xA0${CH_ID}8 0x5
 
 echo "Data raw value:"
-ethercat upload -m $MID -p $SID --type uint64  0xA0$CH_ID0 0x11
+ethercat upload -m $MID -p $SID --type uint64  0xA0${CH_ID}8 0x11
 
 echo ""
 echo "#########################################################"
