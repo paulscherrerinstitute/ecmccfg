f4f447cc4f71ac073bde10a81d458ccedecfead3
Anders Sandström <anders.sandstroem@psi.ch>
2025-03-17 15:05:18 +0100

    PVT: Add trigger cfgs to pvtContrllerConfig.cmd

--- examples/PSI/lab_setup/pvt/test.sh
+++ examples/PSI/lab_setup/pvt/test.sh
@@ -1,7 +1,7 @@
 # pos array
 caput -a c6025a-04:Axis1-PVT-Positions '0 360 720 1080 1440 1080 720'
 # relative
-caput c6025a-04:PVT-MoveMode 1
+caput c6025a-04:PVT-MoveMode 0
 # Use axis
 caput c6025a-04:Axis1-PVT-UseAxis 1
 # build
@@ -10,3 +10,5 @@ caput c6025a-04:PVT-Build 1
 caput c6025a-04:Axis1-TgtPosCmd 1000
 caput c6025a-04:Axis1-MtnCmd 2
 caput c6025a-04:Axis1.CNEN 1
+caput c6025a-04:PVT-EndPulses 5
+caput c6025a-04:PVT-NumPulses 5
