1120cf359ac1d5d93b4fbd6622783f8b6605e004
Anders Sandström <anders.sandstroem@psi.ch>
2025-06-10 17:06:21 +0200

    Add simple panel for master/slave state-machine. Add some PVs

--- scripts/addMasterSlaveSM.cmd
+++ scripts/addMasterSlaveSM.cmd
@@ -18,7 +18,7 @@ ecmcEpicsEnvSetCalc("ECMC_MST_SLV_ID_2_CHAR", "${ECMC_MST_SLV_ID}","%02d")
 
 ecmcConfigOrDie "Cfg.CreateMasterSlaveSM(${ECMC_MST_SLV_ID},${NAME=empty},${MST_GRP_NAME},${SLV_GRP_NAME})"
 ecmcFileExist("ecmcMasterSlaveSM.db",1,1)
-dbLoadRecords("ecmcMasterSlaveSM.db", "P=$(ECMC_PREFIX),ID_2CH=${ECMC_MST_SLV_ID_2_CHAR},ID=${ECMC_MST_SLV_ID},PORT=${ECMC_ASYN_PORT},T_SMP_MS=${ECMC_EC_SAMPLE_RATE_MS}")
+dbLoadRecords("ecmcMasterSlaveSM.db", "P=$(ECMC_PREFIX),ID_2CH=${ECMC_MST_SLV_ID_2_CHAR},ID=${ECMC_MST_SLV_ID},PORT=${ECMC_ASYN_PORT},T_SMP_MS=${ECMC_EC_SAMPLE_RATE_MS},NAME=${NAME=empty},SLV_NAME=${SLV_GRP_NAME},MST_NAME=${MST_GRP_NAME}")
 
 #- increment ID
 ecmcEpicsEnvSetCalc("MST_SLV_ID", "${ECMC_MST_SLV_ID}+1","%d")
