94dd43698dfbf31e609e5b12ecb624ce07f38d50
Anders Sandström <anders.sandstroem@psi.ch>
2025-07-01 09:51:10 +0200

    Update testscripts

--- examples/PSI/best_practice/motion/servo/startup.cmd
+++ examples/PSI/best_practice/motion/servo/startup.cmd
@@ -1,7 +1,7 @@
 ##############################################################################
 ## Example config for ep7211-0034
 
-require ecmccfg "ENG_MODE=1"
+require ecmccfg "ENG_MODE=1,MASTER_ID=1,ECMC_VER=sandst_a"
 
 #- ############################################################################
 #- Configure hardware
@@ -11,7 +11,7 @@ require ecmccfg "ENG_MODE=1"
 #- 15  0:15  PREOP  +  EP7211-0034 1K. MDP742 Servo-Motor-Endstufe mit OCT (50V, 4,5A 
 #- ...
 
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd,        "SLAVE_ID=15, HW_DESC=EP7211-0034_ALL_FB"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd,        "SLAVE_ID=0, HW_DESC=EP7211-0034_ALL_FB"
 #- Limit torque to 50% of motor rated torque.  Rated current = 2710mA, set to half I_MAX_MA=1355
 ${SCRIPTEXEC} ${ecmccfg_DIR}applyComponent.cmd   "COMP=Motor-Beckhoff-AM8111-XFX0, MACROS='I_MAX_MA=1355'"
 $(SCRIPTEXEC) $(ecmccfg_DIR)loadYamlAxis.cmd     "FILE=./cfg/axis.yaml, DRV_ID=$(ECMC_EC_SLAVE_NUM), AX_NAME='Axis1', AX_ID=1"
--- examples/PSI/best_practice/plcs/startup.cmd
+++ examples/PSI/best_practice/plcs/startup.cmd
@@ -1,6 +1,7 @@
 ##############################################################################
 ## Simple example config for plcs
 
+#require ecmccfg sandst_a "ENG_MODE=1,ECMC_VER=sandst_a"
 require ecmccfg "ENG_MODE=1"
 
 #- ############################################################################
@@ -10,7 +11,7 @@ require ecmccfg "ENG_MODE=1"
 #- ...
 #-  10  0:10  PREOP  +  EL2819 16K. Dig. Ausgang 24V, 0,5A, Diagnose
 #- ...
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd,       "SLAVE_ID=10, HW_DESC=EL2819"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd,       "SLAVE_ID=2, HW_DESC=EL2819"
 
 #- ############################################################################
 #- Load PLC (note the include dir (INC) and the use of ECMC_EC_SLAVE_NUM)
