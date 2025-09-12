de44308a2fda9c0004d54f6dc07403a70bbdcc68
Anders Sandström <anders.sandstroem@psi.ch>
2025-09-12 13:43:41 +0200

    Updated test scripts

--- examples/PSI/lab_setup/el7062/openloop_CSP/startup.cmd
+++ examples/PSI/lab_setup/el7062/openloop_CSP/startup.cmd
@@ -1,16 +1,11 @@
 ##############################################################################
 ## Example config for open loop only
-require ecmccfg sandst_a "ENG_MODE=1,ECMC_VER=v10.1.0_RC1"
-require ecmccomp sandst_a
+require ecmccfg v11.0.0_RC1 "ENG_MODE=1,ECMC_VER=v11.0.0_RC1"
+require ecmccomp
 
 ${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd,      "SLAVE_ID=3,HW_DESC=EL7062_CSP"
-${SCRIPTEXEC} ${ecmccfg_DIR}applyComponent.cmd "COMP=Motor-Generic-2Phase-Stepper, CH_ID=1, MACROS='I_MAX_MA=1000, I_STDBY_MA=100, U_NOM_MV=24000, R_COIL_MOHM=2372,L_COIL_UH=3000'"
-${SCRIPTEXEC} ${ecmccfg_DIR}applyComponent.cmd "COMP=Drive-Generic-Ctrl-Params,    CH_ID=1, MACROS='P_KP=20'"
-#${SCRIPTEXEC} ${ecmccfg_DIR}applyComponent.cmd "COMP=Encoder-Generic-INC,          CH_ID=1, MACROS='ST_ENC_RES=1024'"
-#${SCRIPTEXEC} ${ecmccfg_DIR}applyComponent.cmd "COMP=Encoder-Generic-INC,          CH_ID=2, MACROS='ST_ENC_RES=1024,INV_DIR=1'"
+${SCRIPTEXEC} ${ecmccfg_DIR}applyComponent.cmd "COMP=Motor-Generic-2Phase-Stepper, CH_ID=1, MACROS='I_MAX_MA=1000, I_STDBY_MA=100, U_NOM_MV=24000,L_COIL_UH=3050,R_COIL_MOHM=2630'"
+${SCRIPTEXEC} ${ecmccfg_DIR}applyComponent.cmd "COMP=Drive-Generic-Ctrl-Params,    CH_ID=1, MACROS='I_TI=12,I_KP=58,V_TI=150,V_KP=176,P_KP=10'"
 epicsEnvSet(DRV_SID,${ECMC_EC_SLAVE_NUM})
 
 ${SCRIPTEXEC} ${ecmccfg_DIR}loadYamlAxis.cmd,   "FILE=./cfg/axis_open_loop.yaml,    DEV=${IOC}, AX_NAME=M1, AXIS_ID=1, DRV_SID=${DRV_SID}, ENC_SID=${DRV_SID}, ENC_CH=01" 
-
-require ecmc_plugin_motion
-
