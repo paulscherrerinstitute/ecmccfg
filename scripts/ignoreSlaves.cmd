6c777154a0de4d06481a85e1c16e65ed98ca80b9
Anders Sandström <anders.sandstroem@psi.ch>
2025-10-01 17:04:18 +0200

    Rename ignoreSlave.cmd to ingnoreSlaves.cmd. Supports skipping several slaves by macro COUNT

--- scripts/ignoreSlaves.cmd
+++ scripts/ignoreSlaves.cmd
@@ -1,11 +1,12 @@
 #==============================================================================
-# ignoreSlave.cmd
+# ignoreSlaves.cmd
 #- Arguments:
 #-d /**
 #-d   \brief Ignores a slave
 #-d   \details Ignores a slave by increase SLAVE_ID with 1
 #-d   \author Anders Sandström
 #-d   \file
+#-d   \param COUNT (optional) Skip this number of slaves, defaults to 1
 #-d */
 # increment SLAVE_ID
-ecmcEpicsEnvSetCalc("SLAVE_ID", "${SLAVE_ID=0}+1","%d")
+ecmcEpicsEnvSetCalc("SLAVE_ID", "${SLAVE_ID=0}+${COUNT=1}","%d")
