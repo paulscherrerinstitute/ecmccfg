a1c4a5e966554fb61ac09b5713b67b4cc390831b
Anders Sandström <anders.sandstroem@psi.ch>
2025-10-01 17:12:31 +0200

    modify test cfgs

--- examples/PSI/lab_setup/stepper_bissc/startup_local_hw.cmd
+++ examples/PSI/lab_setup/stepper_bissc/startup_local_hw.cmd
@@ -1,10 +1,10 @@
 ##############################################################################
 ## Example config for EL7041 and EL5042
 
-require ecmccfg "ENG_MODE=1,MASTER_ID=0"
+require ecmccfg sandst_a "ECMC_VER=sandst_a,ENG_MODE=1,MASTER_ID=0"
 
 # 0:7 - EL7041    1Ch Stepper
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd,       "SLAVE_ID=14,HW_DESC=EL7041-0052"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd,       "SLAVE_ID=16,HW_DESC=EL7041-0052"
 ${SCRIPTEXEC} ${ecmccfg_DIR}applyComponent.cmd  "COMP=Motor-Generic-2Phase-Stepper,  MACROS='I_MAX_MA=1500, I_STDBY_MA=1000, U_NOM_MV=48000, R_COIL_MOHM=1230'"
 epicsEnvSet(DRV_SID,${ECMC_EC_SLAVE_NUM})
 
