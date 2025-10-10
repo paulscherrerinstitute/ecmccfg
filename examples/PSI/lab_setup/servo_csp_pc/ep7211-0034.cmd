8c6e206569d4bb067eee18de9ad1bd69d8fff43a
Anders Sandström <anders.sandstroem@psi.ch>
2025-10-10 11:55:07 +0200

    Fix servo_csp_pc example

--- examples/PSI/lab_setup/servo_csp_pc/ep7211-0034.cmd
+++ examples/PSI/lab_setup/servo_csp_pc/ep7211-0034.cmd
@@ -33,11 +33,11 @@
 #    needs to be different.
 #
 
-require ecmccfg sandst_a "ENG_MODE=1,ECMC_VER=sandst_a,MASTER_ID=1"
+require ecmccfg sandst_a "ENG_MODE=1,ECMC_VER=v11.0.0_RC1,MASTER_ID=1"
 
-${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd,        "SLAVE_ID=12, HW_DESC=EP7211-0034_CSP_STD"
+${SCRIPTEXEC} ${ecmccfg_DIR}addSlave.cmd,        "SLAVE_ID=0, HW_DESC=EP7211-0034_CSP_STD"
 #- Limit torque to 50% of motor rated torque.  Rated current = 2710mA, set to half I_MAX_MA=1355
-${SCRIPTEXEC} ${ecmccfg_DIR}applyComponent.cmd   "COMP=Motor-Beckhoff-AM8111-XFX0, MACROS='I_MAX_MA=1355'"
+${SCRIPTEXEC} ${ecmccfg_DIR}applyComponent.cmd   "COMP=Motor-Beckhoff-AM8121-XFX0, MACROS='I_MAX_MA=1355'"
 $(SCRIPTEXEC) $(ecmccfg_DIR)loadYamlAxis.cmd     "FILE=./cfg/axis_csp.yaml, DRV_ID=$(ECMC_EC_SLAVE_NUM), AX_NAME='Axis1', AX_ID=1"
 $(SCRIPTEXEC) $(ecmccfg_DIR)loadYamlEnc.cmd      "FILE=./cfg/enc_sim_linear.yaml, DRV_ID=$(ECMC_EC_SLAVE_NUM)"
 
