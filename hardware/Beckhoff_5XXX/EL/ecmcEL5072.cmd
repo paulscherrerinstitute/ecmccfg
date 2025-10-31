db5f254dfabe03895b162e67f645ea7aba4b7015
Anders Sandström <anders.sandstroem@psi.ch>
2025-10-31 11:30:41 +0100

    EL5072: Use new data types to convert int to ints.

--- hardware/Beckhoff_5XXX/EL/ecmcEL5072.cmd
+++ hardware/Beckhoff_5XXX/EL/ecmcEL5072.cmd
@@ -1,6 +1,7 @@
 #-d /**
 #-d   \brief hardware script for EL5072
 #-d   \details 2 channel Inductive sensor interface (LVDT, RVDT)
+#-d            Note: The encoder values are using "S32_TO_U32" that maps -2^31..2^31 to 0..2^32. Better for use with motion axis
 #-d   \author Anders Sandstroem
 #-d   \file
 #-d   \note SDOS
@@ -17,20 +18,24 @@ ${SCRIPTEXEC} ${ecmccfg_DIR}slaveVerify.cmd "RESET=${ECMC_SLAVE_RESET=true}"
 
 #- ###########################################################
 #- ############ Config PDOS: Channel 1
+
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1600,0x7000,0x1,32,encoderControl01)"
-ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1600,0x7000,0x11,32,encoderValue01)"
+
+#- The terminal sends positions as ints, currrently not supported by ecmc axis object (always uints). If interprted as uint this will result in a over/underflow at 0. In order to interpret as uint then use S32_TO_U32.
+#- S32_TO_U32 adds 2^31 to the value effectively maps -2^31..2^31 to 0..2^32. So "S32_TO_U32" ensures it can be linked to ecmc motion object but a different offset is needed.
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1600,0x7000,0x11,S32_TO_U32,encoderValue01)"
 
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x6000,0x1,16,encoderStatus01)"
-ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x6001,0x1,32,positionActual01,1)"
-ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x6001,0x2,32,encoderLatchPostion01,1)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x6001,0x1,S32_TO_U32,positionActual01)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a00,0x6001,0x2,S32_TO_U32,encoderLatchPostion01)"
 
 #- ############ Config PDOS: Channel 2
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1601,0x7010,0x1,32,encoderControl02)"
-ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1601,0x7010,0x11,32,encoderValue02)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},1,2,0x1601,0x7010,0x11,S32_TO_U32,encoderValue02)"
 
 ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a02,0x6010,0x1,16,encoderStatus02)"
-ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a02,0x6011,0x1,32,positionActual02,1)"
-ecmcConfigOrDie "Cfg.EcAddEntryComplete(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a02,0x6011,0x2,32,encoderLatchPostion02,1)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a02,0x6011,0x1,S32_TO_U32,positionActual02)"
+ecmcConfigOrDie "Cfg.EcAddEntryDT(${ECMC_EC_SLAVE_NUM},${ECMC_EC_VENDOR_ID},${ECMC_EC_PRODUCT_ID},2,3,0x1a02,0x6011,0x2,S32_TO_U32,encoderLatchPostion02)"
 
 #- Default panel
 epicsEnvSet("ECMC_HW_PANEL"              "EL5072")
