c9889372608d077d0893232de33f2fc3b0a0d506
Anders Sandström <anders.sandstroem@psi.ch>
2026-05-21 11:08:54 +0200

    Add CSP for ED7062

--- hardware/Beckhoff_7XXX/EL/ecmcED7062_CSP.cmd
+++ hardware/Beckhoff_7XXX/EL/ecmcED7062_CSP.cmd
@@ -1,15 +1,15 @@
 #-d /**
-#-d   \brief hardware script for EL7062 CSV
-#-d   \details EL7062 stepper drive 3ch 8-48V, 3A, max 5A per channel (sum <= 6A)
+#-d   \brief hardware script for ED7062 CSP
+#-d   \details ED7062 stepper drive 3ch 8-48V, 3A, max 5A per channel (sum <= 6A)
 #-d   \author Anders Sandstroem
 #-d   \file
 #-d   \note SDOS
 #-d   \param [out] SDO 0x1011:01 --> 1684107116 \b reset
 #-d */
 
-epicsEnvSet("ECMC_EC_HWTYPE"             "EL7062_CSP")
+epicsEnvSet("ECMC_EC_HWTYPE"             "ED7062_CSP")
 epicsEnvSet("ECMC_EC_VENDOR_ID"          "0x2")
-epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x1b963052")
+epicsEnvSet("ECMC_EC_PRODUCT_ID"         "0x1b961052")
 epicsEnvSet("ECMC_EC_COMP_TYPE"          "EL7062")
 
 #- Load standard cfg
